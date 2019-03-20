459cc0a25ca7b9b4caac844678e180b2a1f196a4
Syed Hussain Ather <shussainather@gmail.com>
2019-03-19 22:06:40 -0400

    def

--- graph/voronoi.py
+++ graph/voronoi.py
@@ -10,4 +10,7 @@ Voronoi diagrams let us divide spcae into several regions such that each region
 has all the points lcoser to one point than to any other seed point.
 """
 
-
+def voronoi(vor, radius=None):
+    """
+    Create infinite Voronoi regions in a 2D diagram to finite regions.
+    """
