5d7746be1bdf3febdce71345ebe7e6d1adfc4f20
Syed Hussain Ather <shussainather@gmail.com>
2019-03-19 22:05:22 -0400

    initial commit

---
+++ graph/voronoi.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import scipy.spatial as spatial
+import matplotlib.pyplot as plt
+import matplotlib.path as path
+import matplotlib as mpl
+
+"""
+Voronoi diagrams let us divide spcae into several regions such that each region
+has all the points lcoser to one point than to any other seed point.
+"""
+
+
