2cd3b8b9b55c72e285f8d45e5c5e028475b1cc41
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 02:03:07 -0400

    sort

--- graph/kruskal.py
+++ graph/kruskal.py
@@ -6,6 +6,11 @@ forest by finding an edge of least weight.
 
 def kruskal(g, w)
     """
-    For graph g and weights w.
+    For graph g (tuple of vertices and edges) and weights w.
     """
-  
+    result = [] # resulting minimum spanning tree (MST) 
+    i = 0 # sorted edges  
+    e = 0 
+    g = sorted(g, key=lambda i:w[i]) # sort by weight
+ 
+    
