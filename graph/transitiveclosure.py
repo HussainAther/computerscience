ed5335fb1c8218e0ccece61e695ec8d6f100b1c5
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 23:24:57 -0400

    initial commit

---
+++ graph/transitiveclosure.py
@@ -0,0 +1,13 @@
+
+"""
+Given a directed graph (V, E) with vertex set V = {1, 2,...n} we might want to determine
+whether G contains a path from i to j for all vertex pairs i, j in the set V. Transitive closure
+of the graph G is G* = (V, E*) where E* = {(i, j): there is ap ath from vertex i to vertex j in G}
+"""
+
+def tc(g):
+    """
+    Compute the matrix T by the Transitive-Closure procedure on a sample graph g that is a tuple
+    of (vertices, edges).
+    """
+    n = 
