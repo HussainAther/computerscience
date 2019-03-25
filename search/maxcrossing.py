34e479b49d3d884824e88b2902aab8d2ddae5c01
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 08:29:14 -0400

    return

--- search/maxcrossing.py
+++ search/maxcrossing.py
@@ -1,3 +1,5 @@
+import sys
+
 """
 Return a tuple of hte indices demarcating a maximum subarray 
 of some array a that crosses the midpoint along with the sum
@@ -6,6 +8,23 @@ fo the values in a maximum subarray.
 
 def fmcs(a):
     """
-    Find max crossing subarray
+    Find max crossing subarray for an array a.
     """
-    
+    l = a[0] # low
+    m = a[len(a)/2] # midpoint 
+    h = a[-1] # high
+    summed = 0 # sum over the values
+    lsum = -sys.maxint  # left sum. we set this as low as we can
+    for i in range(m, l-1, -1):
+         summed += a[i]
+         if summed > lsum:
+             lsum = summed
+             ml = i # max-left 
+    rsum = -sys.maxint
+    summed = 0
+    for i in range(m+1, h+1):
+        summed += a[i]
+        if sum > rsum:
+            rsum = sum
+            mr = i # max-right
+    return (ml, mr, lsum + rsum)
