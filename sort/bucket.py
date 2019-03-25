168f771742b4a4c49081824952b3c23c773fb55a
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 08:03:22 -0400

    ins

--- sort/bucket.py
+++ sort/bucket.py
@@ -2,3 +2,24 @@
 Bucket sort uses the input drawn from a uniform 
 distribution that has an average-case running time of O(n).
 """
+def ins(a):
+    """
+    Insertion sort for array a.
+    """
+    for i in range(1, len(a)):
+        c = a[i]
+        p = i
+        while p > 0 and a[p-1] > c:
+            a[p] = a[p-1]
+            p -= 1 
+
+
+def bucket(a):
+    """
+    Using Bucket sort on an array a
+    """
+    b = [[]]*len(a) # output
+    for i in range(len(a)):
+        b[i].append(a[i])
+    for i in range(len(a)):
+        sort
