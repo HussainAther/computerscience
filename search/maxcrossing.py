80a7d5251dea217b4b5e22c235b1fdf681f2f683
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 08:23:53 -0400

    initial commit

---
+++ search/maxcrossing.py
@@ -0,0 +1,11 @@
+"""
+Return a tuple of hte indices demarcating a maximum subarray 
+of some array a that crosses the midpoint along with the sum
+fo the values in a maximum subarray.
+"""
+
+def fmcs(a):
+    """
+    Find max crossing subarray
+    """
+    
