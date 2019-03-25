44e59149f40e4b712e9d48ff8d9a7b54b62f0fb6
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 01:56:03 -0400

    Def

--- graph/kruskal.py
+++ graph/kruskal.py
@@ -4,4 +4,8 @@ Kruskal's (Kruskal) algorithm finds a safe edge to add to the growing
 forest by finding an edge of least weight.
 """
 
-
+def kruskal(g, w)
+    """
+    For graph g and weights w.
+    """
+  
