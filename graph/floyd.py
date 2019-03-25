444fb8d4267ca5bb030853a024d654053941d9de
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 02:21:44 -0400

    bg

--- graph/floyd.py
+++ graph/floyd.py
@@ -1,6 +1,11 @@
+import sys
+
 """
 We use the Floyd-Warshall algorithm (Floyd Warshall) to find the shortest path
 between all pairs. We find the shortest distances between every pair of vertices
 in a given edge weighted directed graph.
 """
 
+inf = sys.maxint # get as high of a value we can
+
+v = 5 # number of vertices in the graph
