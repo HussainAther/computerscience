b8a80e6d53f48e667bc20b177c4a6539d1860ca7
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 02:11:08 -0400

    initial commit

---
+++ graph/prim.py
@@ -0,0 +1,7 @@
+
+"""
+We use Prim's algorithm to consider
+the edges that connect two sets and pick the minimum weight
+edge from these edges. After picking the edge, move the other
+end-point of hte edge to the set containing the minimum spanning tree. 
+"""
