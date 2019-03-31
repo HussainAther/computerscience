439b6ed554e32664cd14fa5b2e706307299114e3
Syed Hussain Ather <shussainather@gmail.com>
2019-03-30 21:06:18 -0400

    typo

--- graph/prim.py
+++ graph/prim.py
@@ -4,7 +4,7 @@ import sys
 We use Prim's algorithm to consider
 the edges that connect two sets and pick the minimum weight
 edge from these edges. After picking the edge, move the other
-end-point of hte edge to the set containing the minimum spanning tree (MST). 
+end-point of the edge to the set containing the minimum spanning tree (MST). 
 """
 
 def mindist(g, k, mst):
