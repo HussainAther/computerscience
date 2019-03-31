1e61e9ae9d4791d69ec4e37e2deed65c9aa252a0
Syed Hussain Ather <shussainather@gmail.com>
2019-03-31 07:10:58 -0400

    smaller

--- sort/dutchflag.py
+++ sort/dutchflag.py
@@ -11,3 +11,11 @@ def dfp(pivot=0, a):
     For some array of balls a (as described above), partition into the Dutch flag.
     We rely on the pivot index of the array a.
     """
+    p = a[pivot] # pivot value used in first pivot 
+    s = 0 # increment for each pivot which array value we are switching
+          # we begin by looking for elements smaller than our pivot
+    for i in range(len(a)):
+        if a[i] < p:
+            (a[i], a[s]) = (a[s], a[i]) # pivot or switch them
+            s += 1
+    
