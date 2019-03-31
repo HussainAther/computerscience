25a85a06684ff9dd4e26fd6e32ce19530b9b2f96
Syed Hussain Ather <shussainather@gmail.com>
2019-03-31 07:05:53 -0400

    intitial commit

---
+++ sort/dutchflag.py
@@ -0,0 +1,5 @@
+"""
+The Netherlands (Dutch) flag is three stripes of red, white, and blue. Given red, white, and blue balls 
+randomly arranged in a line, arrange them so all balls of the same color are together and their
+collective color groups are in the correct order.
+"""
