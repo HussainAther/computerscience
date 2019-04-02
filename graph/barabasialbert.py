2f1dfe70f30f4fb376a079420e71f95aa96fc3a0
Syed Hussain Ather <shussainather@gmail.com>
2019-04-02 19:16:36 -0400

    initial commit

---
+++ graph/barabasialbert.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+"""
+We use the Barabasi-Albert model (BA barabasi albert) to explain power-law degree distributino
+of networks by considering growth and preferential attachment. The BA algorithm in the BA model
+starts with a small number m of connected nodes and, at every step, adds a new node with n<m edges
+that link the new node ot m different nodes already present in the network.
+"""
