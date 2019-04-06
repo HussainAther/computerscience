4d68c8a565b31ddbb9af844469c0ec9aefd4cfa9
Syed Hussain Ather <shussainather@gmail.com>
2019-04-06 16:05:42 -0400

    spacing

--- sort/insertionsort.py
+++ sort/insertionsort.py
@@ -4,13 +4,12 @@ Insert each new item into it.
 """
 
 def i(listo):
-   for x in range(1,len(listo)):
+    for x in range(1,len(listo)):
+        c = listo[x]
+        p = x
+ 
+        while p > 0 and listo[p-1] > c:
+            listo[p] = listo[p-1]
+            p = p-1
 
-     c = listo[x]
-     p = x
-
-     while p>0 and listo[p-1]>c:
-         listo[p]=listo[p-1]
-         p = p-1
-
-     listo[p]=c
+        listo[p] = c
