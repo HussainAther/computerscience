3ca8d229b75972bd6e207fae8bbb9d7b67180ccd
Syed Hussain Ather <shussainather@gmail.com>
2019-04-06 16:03:43 -0400

    spacing

--- sort/bubblesort.py
+++ sort/bubblesort.py
@@ -3,6 +3,7 @@ Bubble sort makes passes through a list and compares adjacent items
 and exchanges them if they're not in order. Each item forms a bubble around the location
 where it belongs.
 """
+
 def b(listo):
     for p in range(len(listo)-1,0,-1):
         for i in range(p):
