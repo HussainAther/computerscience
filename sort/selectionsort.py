02c939ff1de62bdd7cdd3cb64a0b7ab90b29254e
Syed Hussain Ather <shussainather@gmail.com>
2019-04-06 16:34:52 -0400

    spacing

--- sort/selectionsort.py
+++ sort/selectionsort.py
@@ -3,10 +3,10 @@ Selection sort uses the bubble sort but only one exchange for every pass through
 """
 
 def selectionSort(listo):
-   for f in range(len(listo)-1,0,-1):
-       p=0
-       for l in range(1,f+1):
-           if listo[l]>listo[p]:
+   for f in range(len(listo)-1, 0, -1):
+       p = 0
+       for l in range(1, f+1):
+           if listo[l] > listo[p]:
                p = l
 
        t = listo[f]
