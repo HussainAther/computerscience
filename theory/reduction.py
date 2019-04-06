517da2e8e6f3e31ecca3ea492373b22742d13d15
Syed Hussain Ather <shussainather@gmail.com>
2019-04-06 15:18:13 -0400

    spacing

--- theory/reduction.py
+++ theory/reduction.py
@@ -1,4 +1,3 @@
-
 """
 F reduces to G means, informally, that if you can olve G, you can solve F. Turing reduction can informally
 be stated as F polyreduces to G means if oyu can solve G in polynomial time, you can solve F in polynomial time.
