eafe40ce350a4a77b3b4ed3a6aa2a2fac9f624dc
Syed Hussain Ather <shussainather@gmail.com>
2019-04-11 16:04:41 -0400

    bg

--- theory/ackermann.py
+++ theory/ackermann.py
@@ -1,3 +1,5 @@
 """
 The Ackermann function uses a total computable function that is not primitive function.
+We illustrate the Ackermann (ackermann) function also konwn as the Ackermann-Péter function 
+(peter Peter) which uses two nonnegative integers m and n.
 """
