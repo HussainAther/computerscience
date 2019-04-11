f9d8be8b0fc2a1cac681b68b03eb0c8e051b1881
Syed Hussain Ather <shussainather@gmail.com>
2019-04-11 16:06:34 -0400

    code func

--- theory/ackermann.py
+++ theory/ackermann.py
@@ -1,5 +1,16 @@
 """
 The Ackermann function uses a total computable function that is not primitive function.
 We illustrate the Ackermann (ackermann) function also konwn as the Ackermann-Péter function 
-(peter Peter) which uses two nonnegative integers m and n.
+(peter Peter) which uses two nonnegative integers m and n. We can uuse it optimize recursion.
 """
+
+def ack(m, n):
+    """
+    Iterate through Ackermann function.
+    """
+    if m == 0:
+        return n + 1
+    elif n == 0:
+        return A(m - 1, 1, s)
+    n2 = ack(m, n - 1)
+    return A(m - 1, n2, s)
