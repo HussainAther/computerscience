983e47dfe9ab0fc4cf0452cc842f9e6e24f57abd
Syed Hussain Ather <shussainather@gmail.com>
2019-04-14 22:44:30 -0400

    init var

--- gamesearch/chase.py
+++ gamesearch/chase.py
@@ -9,3 +9,16 @@ import random
 """
 Simple chase game.
 """
+
+# Board size
+P, Q = 80, 150
+
+# Number of chasers and targets
+N_c = 30
+N_t = 100
+
+# Detection distance
+D = np.infty
+
+directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+board = np.zeros((P, Q), dtype=np.uint8)
