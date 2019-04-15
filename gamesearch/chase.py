931055ddb74d86b9fede0cce33838f2d9d1ace57
Syed Hussain Ather <shussainather@gmail.com>
2019-04-14 22:44:03 -0400

    initial commit

---
+++ gamesearch/chase.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.animation import FuncAnimation
+from matplotlib.colors import ListedColormap
+
+import random
+
+"""
+Simple chase game.
+"""
