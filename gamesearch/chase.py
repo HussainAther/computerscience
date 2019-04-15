445aac42e5d8aae8aa3dedb9353afa6f3d4c164e
Syed Hussain Ather <shussainather@gmail.com>
2019-04-14 22:46:26 -0400

    manh dist

--- gamesearch/chase.py
+++ gamesearch/chase.py
@@ -22,3 +22,19 @@ D = np.infty
 
 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 board = np.zeros((P, Q), dtype=np.uint8)
+
+def manhattan((r0, c0), (r1, c1)):
+    """
+    Wrapped manhattan distance between two points on the grid.
+    """
+    if r0 > r1:
+        r0, r1 = r1, r0
+
+    if c0 > c1:
+        c0, c1 = c1, c0
+
+    row_dist = min(r1 - r0, r0 + (P - r1))
+    col_dist = min(c1 - c0, c0 + (Q - c1))
+
+    return row_dist + col_dist
+
