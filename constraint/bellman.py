2b76f7998c17f142018c5ea93425cf67a55d1fa6
Syed Hussain Ather <shussainather@gmail.com>
2019-04-19 13:36:49 -0400

    initial commit

---
+++ constraint/bellman.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+"""
+We use the Bellman equation (bellman) as a necessry condition for optimality
+associated with the mathematical optimization method known as dynamic
+programming.
+"""
