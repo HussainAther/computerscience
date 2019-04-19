7ebec3f0b9edb4f3d0970dcc18f71723bb9b040c
Syed Hussain Ather <shussainather@gmail.com>
2019-04-19 14:16:14 -0400

    title

--- puzzles/gridworld.py
+++ puzzles/gridworld.py
@@ -1,6 +1,8 @@
 import copy
 
 """
+Markov Decision Process(MDP) for Grid world using Value iteration algorithm and Prioritized Sweeping.
+
 GridWorld is a 2D rectangular grid of size (Nrows,Ncolumns) with an agent starting off at one grid cell, 
 moving from cell to cell through the grid, and eventually exiting after collecting a reward. This grid 
 environment is described as follows:
