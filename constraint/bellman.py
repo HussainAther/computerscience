fefdd864de61634cd41b6e3ec1027e1eb4dde777
Syed Hussain Ather <shussainather@gmail.com>
2019-04-19 13:43:49 -0400

    quote

--- constraint/bellman.py
+++ constraint/bellman.py
@@ -3,5 +3,6 @@ import numpy as np
 """
 We use the Bellman equation (bellman) as a necessry condition for optimality
 associated with the mathematical optimization method known as dynamic
-programming.
+programming. Richard Bellman defined the principle of optimality as 
+"Principle of Optimality: An optimal policy has the property that whatever the initial state and initial decision are, the remaining decisions must constitute an optimal policy with regard to the state resulting from the first decision."
 """
