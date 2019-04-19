a4f1e75071c0fb98a411bdaa897a8e4e2f57c7e8
Syed Hussain Ather <shussainather@gmail.com>
2019-04-19 15:57:36 -0400

    comment

--- graph/hca.py
+++ graph/hca.py
@@ -3,10 +3,13 @@ General algorithm for hierchical clustering analysis.
 """
 
 # Returns minimum difference between any pair
-def findMinDiff(arr, m):
+def findMinDiff(a, m):
+    """
+    Return minimum difference between any pair in an array a of length m.
+    """
   
     # Sort array in non-decreasing order
-    arr = sorted(arr)
+    arr = sorted(a)
   
     # Initialize difference as infinite
     diff = 10**20
@@ -22,15 +25,21 @@ def findMinDiff(arr, m):
     # Return min diff
     return diff, min_pair
 
-# Computer the distance between a cluster and a list of clusters
-def computeDistance(c, arr):
+def computeDistance(c, a):
+    """
+    Return the distance between a cluster c and list of clusters a.
+    """
     distances = []
     for i in arr:
         distances.append(i-c)
     return distances
 
 # Construct a graph T by assigning an isolated vertex to each cluster
-def hca(d, n): # n is the number of clusters to form. # d is the data itself in dict form. the keys are the clusters and the items are the elements of them.
+def hca(d, n): 
+    """
+    Create a graph T by assigning an isolated vertex to each cluster for d data in dictionary form and n number of clusters to form.
+    The dictionary keys are the clusters and the items are the elements of them.
+    """
     T = {} # graph to consctruct and output. keys are clusters and items are elements.
     while n > 1:
         diff, closest = findMinDiff(d.keys(), len(d)) # Find the two closest clusters by computing distances and returning the minimum distance and corresponding pair.
