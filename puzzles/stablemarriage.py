787bb03f6b7095e77fc97194df7deb12e26c7f10
Syed Hussain Ather <shussainather@gmail.com>
2019-04-24 22:14:38 -0400

    initial commit

---
+++ puzzles/stablemarriage.py
@@ -0,0 +1,9 @@
+"""
+The stable marriage problem or the stable matching problem (smp SMP).
+Given n men and n women, where each person has ranked all members of the 
+opposite sex in order of preference, marry the men and women together such 
+that there are no two people of opposite sex who would both rather have each 
+other than their current partners. When there are no such pairs of people, 
+the set of marriages is deemed stable.
+"""
+
