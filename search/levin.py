c3323111216c4fcb58877fee17c5b5c1aa5015cd
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 18:05:45 -0400

    typo

--- search/levin.py
+++ search/levin.py
@@ -5,7 +5,7 @@ The Universal Levin (levin) search lets us use the idea that we may run two algo
 at half-speed to determine which one finishes first. We determine computational time
 of algorithms by determining the logarith of the running time of a meta-algorithm
 that verifies whether the output is a solution for the algorithms that it oversees. 
-This Levin complexity is a time-bounded version of Kolmogorov compplexity.
+This Levin complexity is a time-bounded version of Kolmogorov complexity.
 """
 
 def levin(x):
