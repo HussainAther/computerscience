c515c66e74b596e95c08366f76d29b4b8ddacf9b
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 19:16:39 -0400

    comment

--- sort/countingsort.py
+++ sort/countingsort.py
@@ -4,18 +4,21 @@ It works by counting the number of objects having distinct key values (kind of h
 Then doing some arithmetic to calculate the position of each object in the output sequence.
 """
 
-def countSort(arr, exp):
+def countSort(a, exp):
+    """
+    Counting sort for an array a.
+    """
     output = [0 for i in range(256)]
     c = [0 for i in range(256)]
-    ans = ["" for _ in arr]
-    for i in arr:
+    ans = ["" for _ in a]
+    for i in a:
         c[ord(i)] += 1 # ord returns the unicode code point of the character in a string of length one.
     for i in range(256):
         c[i] += c[i-1]
-    for i in range(len(arr)):
-        output[c[ord(arr[i])]-1] = arr[i]
-        c[ord(arr[i])] -= 1
-    for i in range(len(arr)):
+    for i in range(len(a)):
+        output[c[ord(a[i])]-1] = a[i]
+        c[ord(a[i])] -= 1
+    for i in range(len(a)):
         ans[i] = output[i]
     return ans
 
@@ -24,9 +27,12 @@ Radix sort sorts data with integer keys by grouping keys by the individual
 digits which share significant position and value.
 """
 
-def radixSort(arr):
-    max1 = max(arr)
+def radixSort(a):
+    """
+    Group keys by digits to sort.
+    """
+    max1 = max(a)
     exp = 1
     while max1/exp > 0:
-        countSort(arr, exp)
+        countSort(a, exp)
         exp *= 10
