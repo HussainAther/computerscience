1b6a40341ffe7fb8e1b7eaac87079e345a59820e
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 18:00:39 -0400

    bg

--- search/levin.py
+++ search/levin.py
@@ -1,4 +1,6 @@
 """
 The Universal Levin (levin) search lets us use the idea that we may run two algorithms
-at half-speed to determine which one finishes first.
+at half-speed to determine which one finishes first. We determine computational time
+of algorithms by determining the logarith of the running time of a meta-algorithm
+that verifies whether the output is a solution for the algorithms that it oversees. 
 """
