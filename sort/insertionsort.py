421f7f87eae184629d97cfc0881f2b0a78003c0e
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 19:15:13 -0400

    comment

--- sort/insertionsort.py
+++ sort/insertionsort.py
@@ -1,15 +1,17 @@
 """
-Maintain a sorted sublist in the lower ps of the list.
+Maintain a sorted sublist in the lower values of the list.
 Insert each new item into it.
 """
 
 def i(listo):
+    """
+    Insert sort inserts each item by order. 
+    """
     for x in range(1,len(listo)):
         c = listo[x]
         p = x
- 
         while p > 0 and listo[p-1] > c:
             listo[p] = listo[p-1]
             p = p-1
-
         listo[p] = c
+    return listo
