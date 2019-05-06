394b68f3be39f42ffc7c86c336190edb399bd37d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-06 01:21:02 -0400

    comment

--- puzzles/tourist.py
+++ puzzles/tourist.py
@@ -14,16 +14,22 @@ def mantour(n, m, d, r):
     down and right.
     """
 
-    # initialize the distance dictionary
+    # Initialize the distance dictionary
+    # with the first entry being the origin with 
+    # distance 0 
     s = {(0,0):0}
-    
-    for i in range(1,n+1):
+   
+    # For each row, enter distance in the first column
+    # using the down matrix 
+    for i in range(1, n+1):
         s[(i,0)] = s[(i-1,0)] + d[i-1][0]
-        
-    for j in range(1,m+1):
+       
+    # For each column, enter distance in the first row 
+    # using the right matrix
+    for j in range(1, m+1):
         s[(0,j)] = s[(0,j-1)] + r[0][j-1]
 
-    # calculation the distances
+    # Calculate the distances
     for i in range(1,n+1):
         for j in range(1,m+1):
             s(i,j)] = max(s[(i-1,j)] + d[i-1][j], s[(i,j-1)] + r[i][j-1])
