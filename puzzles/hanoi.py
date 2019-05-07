e69e4ac602db36b22575bf30071b83f466235779
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 14:26:16 -0400

    return

--- puzzles/hanoi.py
+++ puzzles/hanoi.py
@@ -6,12 +6,16 @@ move consists of taking the upper disk from one of the stacks and placing it on 
 on an empty rod. No larger disk may be placed on top of a smaller disk.
 """
 
+result = []
+
 def hanoi(n, i, m, f):
     """
     For the number of disks and the numbers representing the initial
-    peg i, middle peg m, and final peg f, solve the Hanoi problem.
-    This function moves n disks from initial to middle, then from
-    middle to final. 
+    peg i, middle peg m, and final peg f (each in list form), solve the 
+    Hanoi problem. This function moves n disks from initial to middle, 
+    then from middle to final. 
     """
     if n > 0:
-        hanoi
+        hanoi(n-1, i, m, f) # perform the Hanoi problem with one fewer disk
+        result.append(i.pop()) # append the final item from i to result
+        move(n-1, m, f, i) # perform the Hanoi problem with one fewer disk from middle to initial
