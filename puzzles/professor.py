95cbe0023bba82c0c85aa88fc85fc932ee1a4647
Syed Hussain Ather <shussainather@gmail.com>
2019-05-06 22:22:05 -0400

    return

--- puzzles/professor.py
+++ puzzles/professor.py
@@ -11,7 +11,23 @@ you to figure out which of the 100 professors are honest (thus identifying
 possible research advisors). It is known that there are more honest than dishonest professors.
 """
 
+honest = []
+dishonest = []
+
 def solve(a):
     """
     For a number of professors a, deduce which professors are honest.
     """
+    pair = []
+    while len(honest) + len(dishonst) != a:
+        for i in a:
+            if pair == []:
+                pair.append(a)
+            elif len(pair) == 1:
+                pair.append(a) # for each pair 
+                if question(pair) == (h, h): # ask both of them the opinion of each other. if the answer is (honest, honest)
+                    honest.append(pair[0])
+                    honest.append(pair[1])
+    return honest, dishonest
+               
+       
