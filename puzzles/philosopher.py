a288f351d0e4b685c5abad810a8247e2be6fdbf4
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 14:29:15 -0400

    bg

--- puzzles/philosopher.py
+++ puzzles/philosopher.py
@@ -1,6 +1,9 @@
 """
-The Dining Philosopher Problem (dining philosopher) states that K philosophers seated around a 
+The Dining Philosopher Problem (dining philosopher) states that n philosophers seated around a 
 circular table with one chopstick between each pair of philosophers. There is one chopstick between 
 each philosopher. A philosopher may eat if he can pickup the two chopsticks adjacent to him. One 
-chopstick may be picked up by any one of its adjacent followers but not both.
+chopstick may be picked up by any one of its adjacent followers but not both. When a philosopher 
+cannot grab both forks it sits and waits. Eating takes random time, then the philosopher puts the 
+forks down and leaves the dining room. After spending some random time thinking about the nature 
+of the universe, he again becomes hungry, and the circle repeats itself.
 """
