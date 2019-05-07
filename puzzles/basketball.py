b81d3a20fe085b502ef1d38244925111f1e62691
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 14:52:15 -0400

    initial commit

---
+++ puzzles/basketball.py
@@ -0,0 +1,5 @@
+"""
+There are 8 basketballs and 1 scale. 7 of them weigh the same. 1 of them has a 
+different weight, (it's either heavier or lighter). How do you find the odd ball 
+with only 2 weighs?
+"""
