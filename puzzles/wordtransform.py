64c438976b2a24e2a95d37e2a299ebdf67cb82e8
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 01:26:14 -0400

    def

--- puzzles/wordtransform.py
+++ puzzles/wordtransform.py
@@ -6,3 +6,12 @@ To transform head into tail one can use four intermediates: head → heal → te
 We say that two words v and w are equivalent if v can be transformed into w by 
 substituting individual letters in such a way that all intermediate words are English words present in an English dictionary.
 """
+
+def eqwords(a, b):
+    """
+    Given two words a and b, find out whether they're equivalent.
+    We may find the Lewis Carroll distance between a and b as the 
+    smallest number of substitutions needed to transform a into v
+    with all intermediate words still being words in the dictionary.
+    If a and b are not equivalent, return False.
+    """
