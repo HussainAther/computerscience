54de5f8e011c655538ee28bdd51aa35d51ed845b
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 01:22:45 -0400

    initial commit

---
+++ puzzles/wordtransform.py
@@ -0,0 +1,8 @@
+"""
+In 1879, Lewis Carroll proposed the following puzzle to the readers of Vanity Fair: 
+transform one English word into another by going through a series of intermediate English 
+words, where each word in the sequence differs from the next by only one substitution. 
+To transform head into tail one can use four intermediates: head → heal → teal → tell → tall → tail. 
+We say that two words v and w are equivalent if v can be transformed into w by 
+substituting individual letters in such a way that all intermediate words are English words present in an English dictionary.
+"""
