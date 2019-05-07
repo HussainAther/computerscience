b59d3d873489327bc486b3cfdfe55715c257c0a0
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 14:20:43 -0400

    initial commit

---
+++ puzzles/hanoi.py
@@ -0,0 +1,7 @@
+"""
+In the Tower of Hanoi (tower hanoi) puzzle, we have three rods and a number of disks of different sizes, which can 
+slide onto any rod. The puzzle starts with the disks in a neat stack in ascending order of size on one rod, the 
+smallest at the top. We must move the entire stack to another rod. Only one disk can be moved at a time. Each 
+move consists of taking the upper disk from one of the stacks and placing it on top of another stack or 
+on an empty rod. No larger disk may be placed on top of a smaller disk.
+"""
