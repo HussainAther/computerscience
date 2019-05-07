1bd77a5d64cfde40eaf17749ba8609c370f2e7e7
Syed Hussain Ather <shussainather@gmail.com>
2019-05-07 14:57:59 -0400

    def

--- puzzles/basketball.py
+++ puzzles/basketball.py
@@ -1,5 +1,20 @@
 """
-There are 8 basketballs and 1 scale. 7 of them weigh the same. 1 of them has a 
-different weight, (it's either heavier or lighter). How do you find the odd ball 
-with only 2 weighs?
+There are 8 basketballs and 1 scale. 7 of them weigh the same. 1 of them is heavier. 
+How do you find the odd ball with only 2 weighs?
 """
+
+"""
+We place 3 balls on the left scale with 3 on the right. Of the three scenarios (1) 
+If the left side is heavier, we know one of them on the left is the heavier ball. 
+Then, we weigh the first and second ball on the left to determine which one it is 
+or whether it's the one not weighed. (2) If the right side is heavier, then we perform
+the same thing, but, instead, using the balls on the right. (3) If the two sides
+are equal, we can deduce the heavier ball by weighing the remaining two balls. 
+"""
+
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+
+def solve():
+    """
+    Solve as dictated above.
+    """
