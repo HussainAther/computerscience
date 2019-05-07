9ab1275bf6bf59b74ae02758c9981aa083562e84
Syed Hussain Ather <shussainather@gmail.com>
2019-05-06 22:02:51 -0400

    initial commit

---
+++ puzzles/professor.py
@@ -0,0 +1,14 @@
+"""
+A very large bioinformatics department at a prominent university 
+has a mix of 100 professors: some are honest and hard-working, 
+while others are deceitful and do not like students. The honest 
+professors always tell the truth, but the deceitful ones sometimes 
+tell the truth and sometimes lie. You can ask any professors the 
+following question about any other professor: "Professor Y, is 
+Professor X honest?" Professor Y will answer with either "yes" or "no." 
+Design an algorithm that, with no more than 198 questions, would allow 
+you to figure out which of the 100 professors are honest (thus identifying 
+possible research advisors). It is known that there are more honest than dishonest professors.
+"""
+
+
