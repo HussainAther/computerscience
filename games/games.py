caaba266a1d9808912876208b22c78900adde4b7
Syed Hussain Ather <shussainather@gmail.com>
2019-05-10 23:39:02 -0400

    compute

--- games/games.py
+++ games/games.py
@@ -262,3 +262,31 @@ class TicTacToe(Game):
         for x in range(1, self.h+1):
             for y in range(1, self.v+1):
                 print(board.get((x, y), ".")),
+
+    def compute_utility(self, board, move, player):
+        """
+        If X wins with this move, return 1; if O return -1; else return 0.
+        """
+        if (self.k_in_row(board, move, player, (0, 1)) or
+            self.k_in_row(board, move, player, (1, 0)) or
+            self.k_in_row(board, move, player, (1, -1)) or
+            self.k_in_row(board, move, player, (1, 1))):
+            return if_(player == "X", +1, -1)
+        else:
+            return 0
+
+    def k_in_row(self, board, move, player, (delta_x, delta_y)):
+        """
+        Return true if there is a line through move on board for player.
+        """
+        x, y = move
+        n = 0 # n is number of moves in row
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x + delta_x, y + delta_y
+        x, y = move
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x - delta_x, y - delta_y
+        n -= 1 # Because we counted move itself twice
+        return n >= self.k
