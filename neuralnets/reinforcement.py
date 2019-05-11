6be29ce5c6b118afeba5644dc361782821047adb
Syed Hussain Ather <shussainather@gmail.com>
2019-05-11 00:19:17 -0400

    def

--- neuralnets/reinforcement.py
+++ neuralnets/reinforcement.py
@@ -1,4 +1,10 @@
 """
 We use reinforcement learning to create agents that perform actions in environments with
-rewards depending on the state of the agent.
+rewards depending on the state of the agent. We may use a reward table that has states
+the agent may be in such that the agent gains a reward based on the actions.
 """
+
+def reward(env, ep=500):
+    """
+    Reward the actions in each state.
+    """ 
