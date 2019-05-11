03fff93d4864506706957ff3988117c6056307b4
Syed Hussain Ather <shussainather@gmail.com>
2019-05-11 00:35:29 -0400

    import

--- neuralnets/reinforcement.py
+++ neuralnets/reinforcement.py
@@ -1,3 +1,14 @@
+import numpy as np
+import gym
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+
 """
 We use reinforcement learning to create agents that perform actions in environments with
 rewards depending on the state of the agent. We may use a reward table that has states
