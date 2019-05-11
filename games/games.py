a11b67eca287a355eadb99e1d7bde25bdfd4088a
Syed Hussain Ather <shussainather@gmail.com>
2019-05-10 23:27:48 -0400

    return action

--- games/games.py
+++ games/games.py
@@ -111,3 +111,10 @@ def alphabeta_search(state, game, d=4, cutoff_test=None, eval_fn=None):
                 return v
             beta = min(beta, v)
         return v
+
+    cutoff_test = (cutoff_test or
+                   (lambda state,depth: depth>d or game.terminal_test(state)))
+    eval_fn = eval_fn or (lambda state: game.utility(state, player))
+    action, state = argmax(game.successors(state),
+                           lambda ((a, s)): min_value(s, -infinity, infinity, 0))
+    return action
