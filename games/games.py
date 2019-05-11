9cd7702b06e1f8294b13ff88af6dc851a2c2eb66
Syed Hussain Ather <shussainather@gmail.com>
2019-05-10 23:14:19 -0400

    minimax

--- games/games.py
+++ games/games.py
@@ -1,2 +1,14 @@
 from utils import *
 import random
+
+"""
+Python implementation of vairous search processes in games.
+"""
+
+def minimax_decision(state, game):
+    """
+    Minimax search. Given a state in a game, calculate the best move by searching
+    forward all the way to the terminal states.
+    """
+    player = game.to_move(state)
+
