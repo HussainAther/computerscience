ddcc339d7d81d58341d63981a4f7ba8276f7237d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-11 01:09:57 -0400

    test keras

--- neuralnets/reinforcement.py
+++ neuralnets/reinforcement.py
@@ -23,5 +23,24 @@ def reward(games=500):
     table = np.zeros((5, 2)) # Initialize the table
 
 """
-Use keras.
+Use keras. We will be using Deep Q-learning algorithm. Q-learning is a 
+policy based learning algorithm with the function approximator as a neural 
+network. This algorithm was used by Google to beat humans at Atari games.
 """
+
+ENV_NAME = "CartPole-v0"
+
+# Get the environment and extract the number of actions available in the Cartpole problem
+env = gym.make(ENV_NAME)
+np.random.seed(123)
+env.seed(123)
+nb_actions = env.action_space.n
+
+# Build a single-layer hidden neural network
+model = Sequential()
+model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+model.add(Dense(16))
+model.add(Activation("relu"))
+model.add(Dense(nb_actions))
+model.add(Activation("linear"))
+print(model.summary())
