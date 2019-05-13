72558333a7ab790f78d0b5032df6ebaf77913a3e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-13 00:24:33 -0400

    def

--- graph/mccreight.py
+++ graph/mccreight.py
@@ -1,3 +1,9 @@
 """
 McCreight's algorithm (McCreight mccreight) builds a suffix tree in linear time.
 """
+
+def mccreight(t):
+    """
+    For an input list of suffixes t, build a suffix tree that repressents
+    all possible suffixes of t.
+    """
