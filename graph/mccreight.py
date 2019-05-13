345be1fdc3e80cbf5ce40d75bdd6e4de7204bc57
Syed Hussain Ather <shussainather@gmail.com>
2019-05-13 00:32:20 -0400

    1st and 2nd

--- graph/mccreight.py
+++ graph/mccreight.py
@@ -7,7 +7,8 @@ def mccreight(a):
     For an input list of suffixes a, build a suffix tree that repressents
     all possible suffixes of a.
     """
-    t = {0: "root"} # start with root node
-    for i in a:
+    t = {0: "root"} # start with root node. key for level. value for node.
+    for i, j in enumerate(a):
         d = len(t) # depth
-         
+        t[d+1] = i # add the first node
+        t[d+1] = j # add the second node
