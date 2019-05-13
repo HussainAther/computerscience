f1f3d9dbdc2b2f528c92394ceb2097628da2fab0
Syed Hussain Ather <shussainather@gmail.com>
2019-05-13 00:28:54 -0400

    dict

--- graph/mccreight.py
+++ graph/mccreight.py
@@ -7,5 +7,7 @@ def mccreight(a):
     For an input list of suffixes a, build a suffix tree that repressents
     all possible suffixes of a.
     """
-    t = ["root"] # start with root node
-     
+    t = {0: "root"} # start with root node
+    for i in a:
+        d = len(t) # depth
+         
