df91e74ba927dd5e354704d90364c26958edab37
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 14:41:21 -0400

    node class

--- graph/suffixtree.py
+++ graph/suffixtree.py
@@ -1,3 +1,23 @@
 """
 Build a suffix tree. 
 """
+
+class SuffixNode:
+     """
+     Node class for constructing node-based graphs.
+     """
+     def __init__(self, suffix_link = None):
+         """
+         Initialize the node with its corresponding suffix links.
+         """
+         self.children = {}
+         if suffix_link is not None:
+             self.suffix_link = suffix_link
+         else:
+             self.suffix_link = self
+
+     def add_link(self, c, v):
+          """
+          Link this node to node v via string c.
+          """
+          self.children[c] = v
