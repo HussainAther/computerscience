532b0c91e644006a47a14aee5a9b75fb485da27a
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 05:25:01 -0400

    initial commit

---
+++ puzzles/nucleotide.py
@@ -0,0 +1,6 @@
+"""
+Two players play the following game with a nucleotide sequence of length n. 
+At every turn a player may delete either one or two nucleotides from the sequence. 
+The player who deletes the last letter wins. Who will win? Describe the winning 
+strategy for each n.
+"""
