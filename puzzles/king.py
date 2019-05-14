7f9a8374f79c069f2313f92b0c2649b19151f92d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 02:03:27 -0400

    explain

--- puzzles/king.py
+++ puzzles/king.py
@@ -7,5 +7,14 @@ king on the lower right square of the chessboard wins. Who will win? Describe th
 
 def king(p)
     """
-    For some starting tuple p, determine if player one will win.
+    For some starting tuple position p, determine if player one will win.
+    
+    We notice that player one can force player two to lose moving
+    directly to position g2, right above the bottom-right square.
+    At f3, player one also will lose. Moving either right or down 
+    by one or two lets player two win. This continues until the 
+    top-left so that, for plyaer two to win, player two should
+    make his/her first move by moving the king to the diagnol. This
+    means player one will move into a losing position for the next turn. 
     """
+    
