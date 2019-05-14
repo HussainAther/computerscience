5e037cdca389b91208e087a5ccc70458103a5375
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 02:07:02 -0400

    def

--- puzzles/dna.py
+++ puzzles/dna.py
@@ -3,3 +3,9 @@ Assume you have a DNA base sequence P of n bases and are given
 a sequence S of m bases. The problem is to find the place in S where P first
 occurs, if there is one.
 """
+
+def naive(P, S):
+    """
+    Naïve exhaustive search (naive Naive) algorithm for P sequence
+    and S sequence.
+    """
