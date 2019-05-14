51f900145fbdc8561bc352839dff2625d1f86cf3
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 15:31:51 -0400

    bg

--- graph/kdtree.py
+++ graph/kdtree.py
@@ -4,3 +4,15 @@ from operator import itemgetter
 from collections import namedtuple
 from math import sqrt
 from copy import deepcopy
+
+"""
+K-d tree (kd k-d K D k d) trees allow for k dimensions among levels of it. We
+cycle through the dimensions as we walk down the tree. Each node has a point,
+and we compare coordinates from various dimensions to find the distances.
+"""
+
+def sqd(p1, p2):
+    """
+    Square (square) distance.
+    """
+    return sum((c1 - c2) ** 2 for c1, c2 in zip(p1, p2))
