d9f97886ce44bbb2c111bf5f5acf293e74a1ceff
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 14:52:05 -0400

    init random

--- search/gibbs.py
+++ search/gibbs.py
@@ -1,3 +1,5 @@
+import random
+
 """
 Motif finding (motif) with Gibbs (gibbs) sampling.
 """
@@ -6,3 +8,5 @@ def gibbs(seqs):
     """
     For a list of sequences seqs, find the best motif using Gibbs sampling.
     """
+    k = len(seqs)
+    I = [random.randint(0, len(x) - k) for x in Seqs] 
