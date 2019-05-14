6a696d8b86033d0a17608c284abb82903bd77888
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 05:16:22 -0400

    initial commit

---
+++ puzzles/queen.py
@@ -0,0 +1,7 @@
+"""
+A queen stands on the third square of the uppermost row of a chessboard. Two 
+players take turns moving the queen either horizontally to the right or vertically 
+downward or diagonally in the southeast direction (as many squares as they want). 
+The player who can place the queen on the lower right square of the chessboard 
+wins. Who will win? Describe the winning strategy.
+"""
