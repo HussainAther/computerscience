6ed46142d88c75cccc4cebf4e43f166e8b9a2aae
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 17:04:26 -0400

    initial commit

---
+++ graph/sternbrocot.py
@@ -0,0 +1,7 @@
+"""
+In number theory, the Stern–Brocot tree is an infinite complete binary tree 
+in which the vertices correspond one-for-one to the positive rational numbers, 
+whose values are ordered from the left to the right as in a search tree.
+
+stern brocot Stern Brocot
+"""
