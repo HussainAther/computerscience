4ff65ed84e576062e8f30f833fb3a19f3eb4f141
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 01:06:33 -0400

    initial commit

---
+++ puzzles/chameleons.py
@@ -0,0 +1,8 @@
+"""
+There are n black, m green, and k brown chameleons on a deserted island. 
+When two chameleons of different colors meet they both change their color 
+to the third one (e.g., black and green chameleons become brown). For each 
+choice of n, m, and k decide whether it is possible that after some time 
+all the chameleons on the island are the same color (if you think that it 
+is always possible, check the case n = 1, m = 3, and k = 5).
+"""
