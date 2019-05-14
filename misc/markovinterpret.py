2d6c0e18985923cda37f3000bcb2b8b20660ca14
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 16:55:27 -0400

    def

--- misc/markovinterpret.py
+++ misc/markovinterpret.py
@@ -1,3 +1,5 @@
+import re
+
 """
 A Markov interpreter.
 
@@ -10,3 +12,26 @@ The example gains flexibility by not being tied to specific files. The functions
 may be imported into other programs which then can provide textual input from their 
 sources without the need to pass "file handles" around.
 """
+
+def extractreplacements(grammar):
+    """
+    For re syntax, extract the replacements.
+    """
+    return [ (matchobj.group("pat"), matchobj.group("repl"), bool(matchobj.group("term")))
+                for matchobj in re.finditer(syntaxre, grammar)
+                if matchobj.group('rule')]
+ 
+def replace(text, replacements):
+    """
+    Replace text with replacements.
+    """
+    while True:
+        for pat, repl, term in replacements:
+            if pat in text:
+                text = text.replace(pat, repl, 1)
+                if term:
+                    return text
+                break
+        else:
+            return text
+ 
