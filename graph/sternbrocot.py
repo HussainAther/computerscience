96b07719f953c1fd1da17031850c7239a040583a
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 17:06:32 -0400

    def

--- graph/sternbrocot.py
+++ graph/sternbrocot.py
@@ -1,3 +1,7 @@
+from itertools import takewhile, tee, islice
+from collections import deque
+from fractions import gcd
+
 """
 In number theory, the Stern–Brocot tree is an infinite complete binary tree 
 in which the vertices correspond one-for-one to the positive rational numbers, 
@@ -5,3 +9,15 @@ whose values are ordered from the left to the right as in a search tree.
 
 stern brocot Stern Brocot
 """
+
+def stern_brocot():
+    """
+    We iterate to produce successive members of the sequence. We check the gcd's
+    (Greatest common denominators greatest denominators) to find two iterators
+    that we tee off from the one stream with the second advanced by one value with 
+    the next() call.
+    """
+    sb = deque([1, 1])
+    while True:
+        sb += [sb[0] + sb[1], sb[1]]
+        yield sb.popleft()
