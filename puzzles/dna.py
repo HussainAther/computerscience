af47e55716f11971361b236c3f594cc79ec84928
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 02:08:50 -0400

    return

--- puzzles/dna.py
+++ puzzles/dna.py
@@ -7,5 +7,14 @@ occurs, if there is one.
 def naive(P, S):
     """
     Naïve exhaustive search (naive Naive) algorithm for P sequence
-    and S sequence.
+    and S sequence. Find the location i or output False otherwise.
     """
+    n = len(P)
+    m = len(S)
+    for i in range(n-m+1):
+        for j in range(m):
+            if S[i+j] != P[j]:
+                break
+            if j == len(m):
+                return i
+    return False 
