e2fcde874b1148ae7cc0e75f2944836c23337d7d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 01:23:36 -0400

    initial commit

---
+++ puzzles/bacteria.py
@@ -0,0 +1,9 @@
+"""
+There are n bacteria and 1 virus in a Petri dish. Within the first minute, the virus kills 
+one bacterium and produces another copy of itself, and all of the remaining bacteria reproduce, 
+making 2 viruses and 2 · (n − 1) bacteria. In the second minute, each of the viruses kills 
+a bacterium and produces a new copy of itself (resulting in 4 viruses and 2(2(n − 1) − 2) = 4n − 8 
+bacteria; again, the remaining bacteria reproduce. This process continues every minute. Will 
+the viruses eventually kill all the bacteria? If so, design an algorithm that computes how 
+many steps it will take. How does the running time of your algorithm depend on n?
+"""
