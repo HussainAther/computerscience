7cdee1fea24e76a4079fe55ce8bae1e1a0b59c00
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 01:57:37 -0400

    initial commit

---
+++ puzzles/king.py
@@ -0,0 +1,6 @@
+"""
+A king stands on the upper left square of the chessboard. Two players make turns 
+moving the king either one square to the right or one square downward or one 
+square along a diagonal in the southeast direction. The player who can place the 
+king on the lower right square of the chessboard wins. Who will win? Describe the winning strategy.
+"""
