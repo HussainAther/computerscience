cc85c510ebac857cd10f7a30d185c7688b5345df
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 05:26:01 -0400

    def

--- puzzles/nucleotide.py
+++ puzzles/nucleotide.py
@@ -4,3 +4,8 @@ At every turn a player may delete either one or two nucleotides from the sequenc
 The player who deletes the last letter wins. Who will win? Describe the winning 
 strategy for each n.
 """
+
+def nucleotide(n):
+    """
+    For a starting length n, player one will definitely win. Player one 
+    must 
