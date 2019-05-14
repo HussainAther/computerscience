c0c85e7e4f711894cf518a37fe2aa157ca61d9a5
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 01:33:53 -0400

    initial commit

---
+++ puzzles/naturalnumbers.py
@@ -0,0 +1,7 @@
+"""
+You are given an 8 × 8 table of natural numbers. In any one step, you can 
+either double each of the numbers in any one row, or subtract 1 from each 
+of the numbers in any one column. Devise an algorithm that transforms the 
+original table into a table of all zeros. What is the running time of your 
+algorithm?
+"""
