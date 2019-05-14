16016b032da0d723d263c935fb1deab941c559bd
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 16:43:12 -0400

    import and init

--- theory/forestfire.py
+++ theory/forestfire.py
@@ -1,5 +1,17 @@
+import random
+
+try: # Discern python version for input
+    raw_input
+except:
+    raw_input = input
+
 """
 Forest-Fire Cellular automation http://en.wikipedia.org/wiki/Forest-fire_model
 
 forest fire Forest Fire
 """
+
+L = 15
+initial_trees = 0.55
+p = 0.01
+f = 0.001
