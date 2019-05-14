955ab0a4f839421424f1f50f63b3d07a2c6e55ce
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 14:53:34 -0400

    profile

--- search/gibbs.py
+++ search/gibbs.py
@@ -9,4 +9,9 @@ def gibbs(seqs):
     For a list of sequences seqs, find the best motif using Gibbs sampling.
     """
     k = len(seqs)
-    I = [random.randint(0, len(x) - k) for x in Seqs] 
+    I = [random.randint(0, len(x) - k) for x in Seqs]
+    lastI = None
+    while I != LastI:
+        lastI = list(I)
+        for i in xrange(len(seqs)):
+            p = profile_for(x[j : j + k] for q, (x, j) in enumerate(zip(Seqs, I)) if q != i]) 
