d300d226f58c9c931a57aa2d5ef4dbcf4a373f63
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 14:56:17 -0400

    return

--- search/gibbs.py
+++ search/gibbs.py
@@ -4,14 +4,23 @@ import random
 Motif finding (motif) with Gibbs (gibbs) sampling.
 """
 
-def gibbs(seqs):
+def gibbs(seqs, k):
     """
-    For a list of sequences seqs, find the best motif using Gibbs sampling.
+    For a list of sequences seqs, find the best motif using Gibbs sampling
+    for substring length k.
     """
-    k = len(seqs)
     I = [random.randint(0, len(x) - k) for x in Seqs]
     lastI = None
     while I != LastI:
         lastI = list(I)
         for i in xrange(len(seqs)):
-            p = profile_for(x[j : j + k] for q, (x, j) in enumerate(zip(Seqs, I)) if q != i]) 
+            p = profile_for(x[j : j + k] for q, (x, j) in enumerate(zip(Seqs, I)) if q != i])
+            best = None
+            for j in xrange(len(seqs[i]) - k + 1):
+                score = profile_score(P, seqs[i][j:j+l])
+                if score > best or best is None:
+                    best = score
+                    bestpos = j
+             I[i] = bestpos
+    return I, [x[j : j + k] for x, j in zip(Seqs, I)]
+ 
