6ba65322670ef642d069a12003c0893b53891040
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 17:18:25 -0400

    bg

--- misc/sqlauthentication.py
+++ misc/sqlauthentication.py
@@ -5,5 +5,6 @@ import sys
 import random	
 
 """
-
+Connect to a MySQL database (connect_db), create user/password records in the following table (create_user), and
+authenticate login requests against the table (authenticate_user).
 """
