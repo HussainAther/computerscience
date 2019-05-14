8cbc42915656f882778b9ec73a52b82e893ccb10
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 17:50:29 -0400

    initial commit

---
+++ misc/plasmaeffect.py
@@ -0,0 +1,5 @@
+"""
+The plasma effect is a computer-based visual effect animated in real-time. 
+It uses cycles of changing colours warped in various ways to give an illusion 
+of liquid, organic movement.
+"""
