bb3139f818d278a4a7718b72266df0dfd91731c7
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 00:47:41 -0400

    bg

--- puzzles/rocks.py
+++ puzzles/rocks.py
@@ -1,2 +1,5 @@
 """
+This is a game between two players. It starts with two piles of rocks, ten chips per pile. On each turn, a
+player may take one chip from one pile, or two chips, one from each pile. If one pile is empty, you can
+only take one chip from the other pile. The player who takes the last chip or chips wins the game.
 """
