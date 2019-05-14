2f410466e355a1267a26685a5ce412a52f2a1ffd
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 06:09:19 -0400

    lcs and lcsl

--- puzzles/substring.py
+++ puzzles/substring.py
@@ -4,27 +4,47 @@ Find the longest common substring between two strings.
 subsequence sequence
 """
 
-def lcs(x, y): 
+def lcs(x, y):
     """
+    Return the longest common substring (subsequence)
+    between two strings x and y.
+    """
+    result = ""
+    m, n = len(x, len(y)
+    for i in range(m):
+        match = ""
+        for j in range(y):
+            if (i + j < m and x[i + j] == y[j]):
+                match += y[j]
+            else:
+                if (len(match) > len(result)): result = match
+                match = ""
+    return result
+
+def lcsl(x, y): 
+    """
+    Return the length of the longest common substring.
     Use a table to store lengths of the longest common
     suffixes of substrings. The longest common suffix of 
     x and y. The first row and first column entries have
     no logical meaning.
     """
-    m = len(x)
-    n = len(y)
-    suff = [[0 for k in range(n+1)] for l in range(m+1)] # suffix 
+    m, n = len(x), len(y)
+    suff = [[0 for k in range(n+1)] for l in range(m+1)] # suffix table 
     result = 0 
-    for i in range(m + 1): 
-        for j in range(n + 1): 
+    for i in range(m+1): 
+        for j in range(n+1): 
             if (i == 0 or j == 0): 
                 suff[i][j] = 0
             elif (x[i-1] == y[j-1]): 
                 suff[i][j] = suff[i-1][j-1] + 1
                 result = max(result, suff[i][j]) 
             else: 
-                sufff[i][j] = 0
-    return result 
+                suff[i][j] = 0
+    return result
+    
+
+lcs("baab", "aa") 
 
 """
 Find the shortest superstring (supersequence).
