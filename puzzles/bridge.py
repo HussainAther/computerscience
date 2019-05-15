97fef659e90a474a887561f7b8fe62ae94cd9200
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 20:54:52 -0400

    walk

--- puzzles/bridge.py
+++ puzzles/bridge.py
@@ -9,3 +9,13 @@ map = { "a" : ["c", "d"],
         "c" : ["a", "b", "d"],
         "d" : ["a", "b", "c"]
        }
+
+walk_len = 2 # to cover all nodes
+start_node = "a" # which node to begin
+walk = [[start_node]] # where we have walked
+for _ in range(walk_len + 1):
+    next_nodes = list()
+    for node in walk[-1]:
+        next_nodes.extend(map[node])
+    walk.append(next_nodes)
+walk = walk[1:]
