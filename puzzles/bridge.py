3aca22582f098795ffe96ef938218ad5544ee0fc
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 20:53:38 -0400

    map

--- puzzles/bridge.py
+++ puzzles/bridge.py
@@ -2,3 +2,10 @@
 The Seven Bridges of Königsberg (Konisberg konisberg seven bridges) involves devising a plan
 to cross each bridge in a city once and only once.
 """
+
+# Map each node to its connecting nodes using bridges
+map = { "a" : ["c", "d"],
+        "b" : ["c", "d"],
+        "c" : ["a", "b", "d"],
+        "d" : ["a", "b", "c"]
+       }
