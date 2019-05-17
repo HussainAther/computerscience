203b93ad52e2d80380abf50699b20b35cb856913
Syed Hussain Ather <shussainather@gmail.com>
2019-05-17 00:46:26 -0400

    import

--- graph/vertexcover.py
+++ graph/vertexcover.py
@@ -1,4 +1,9 @@
+import itertools
+
 """
 A vertex-cover (vertex cover vertexcover) of an undirected graph (G=(V, E)) is a subset
 V' of V such that, if edge (u, v) is an edge of G, then u is in V' or v is in V' or both.
+The set V' is said to "cover" the edges of G. A minimum vertex cover is a vertex cover
+of the smallest possible size. If every vertex has a cost, we want to minimize the total
+cost while covering every edge of the graph.  
 """
