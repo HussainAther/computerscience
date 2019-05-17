60125c97ab37b79007cd1798a62988bfae9b9217
Syed Hussain Ather <shussainather@gmail.com>
2019-05-17 01:46:07 -0400

    transfer

--- neuralnets/backprop.py
+++ neuralnets/backprop.py
@@ -1,8 +1,10 @@
+import numpy as np
+
 from random import seed
 from random import random
 
 """
-Backpropagation used for classification are part of the supervised learning
+Neural network backpropagation used for classification are part of the supervised learning
 method for multilayer feed-forward artificial neural networks. Using the ways
 neural cells process informaiton, we model a given function by modifying internal
 weights of inputs signals to give an output signal. We use the error between output and 
@@ -28,3 +30,21 @@ seed(1234)
 network = initialize_network(2, 1, 2)
 for layer in network:
     print(layer)
+
+
+def activate(weights, inputs):
+    """
+    Calculate neuron activation for weights and input.
+    Used in forward propagation.
+    """
+    activation = weights[-1]
+    for i in range(len(weights)-1):
+        activation += weights[i] * inputs[i]
+    return activation
+
+def transfer(activation):
+    """
+    Transfer the activation to output using the sigmoid
+    activation lgoistic function. 
+    """
+    return 1.0 / (1.0 + np.exp(-activation))
