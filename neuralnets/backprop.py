ba155b2a5583b85fa1c65e17afafb63920ed2de1
Syed Hussain Ather <shussainather@gmail.com>
2019-05-17 01:24:44 -0400

    bg

--- neuralnets/backprop.py
+++ neuralnets/backprop.py
@@ -1,6 +1,9 @@
+
+
 """
 Backpropagation used for classification are part of the supervised learning
 method for multilayer feed-forward artificial neural networks. Using the ways
 neural cells process informaiton, we model a given function by modifying internal
-weights of inputs signals to give an output signal.
+weights of inputs signals to give an output signal. We use the error between output and 
+a known expected output to modify the internal state.
 """
