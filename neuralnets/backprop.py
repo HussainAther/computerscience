066fd8217164ada8a75b39e1c9e17f3eba0e1418
Syed Hussain Ather <shussainather@gmail.com>
2019-05-17 01:36:07 -0400

    import

--- neuralnets/backprop.py
+++ neuralnets/backprop.py
@@ -1,4 +1,5 @@
-
+from random import seed
+from random import random
 
 """
 Backpropagation used for classification are part of the supervised learning
@@ -6,4 +7,8 @@ method for multilayer feed-forward artificial neural networks. Using the ways
 neural cells process informaiton, we model a given function by modifying internal
 weights of inputs signals to give an output signal. We use the error between output and 
 a known expected output to modify the internal state.
+
+The seeds dataset involves the prediction of species given measurements seeds from different varieties of wheat.
+
+There are 201 records and 7 numerical input variables. It is a classification problem with 3 output classes. The scale for each numeric input value vary, so some data normalization may be required for use with algorithms that weight inputs like the backpropagation algorithm.
 """
