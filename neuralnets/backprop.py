9333d78e09ba1968d5d6528c591d4e740d3762f4
Syed Hussain Ather <shussainather@gmail.com>
2019-05-17 01:40:41 -0400

    init nn

--- neuralnets/backprop.py
+++ neuralnets/backprop.py
@@ -12,3 +12,14 @@ The seeds dataset involves the prediction of species given measurements seeds fr
 
 There are 201 records and 7 numerical input variables. It is a classification problem with 3 output classes. The scale for each numeric input value vary, so some data normalization may be required for use with algorithms that weight inputs like the backpropagation algorithm.
 """
+
+def initialize_network(n_inputs, n_hidden, n_outputs):
+    """
+    Initialize a neural network with inputs, hidden neurons, and outputs.
+    """
+    network = list()
+    hidden_layer = [{"weights" : [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    network.append(hidden_layer)
+    output_layer = [{"weights" : [random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    network.append(output_layer)
+    return network
