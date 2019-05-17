abbe5abdea7f398aea33515ced96405f507ac33b
Syed Hussain Ather <shussainather@gmail.com>
2019-05-17 00:44:40 -0400

    bg

--- graph/vertexcover.py
+++ graph/vertexcover.py
@@ -1,4 +1,4 @@
 """
 A vertex-cover (vertex cover vertexcover) of an undirected graph (G=(V, E)) is a subset
-V' of V
+V' of V such that, if edge (u, v) is an edge of G, then u is in V' or v is in V' or both.
 """
