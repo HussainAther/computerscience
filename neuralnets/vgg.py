82430adae2f84d93a8c5515025240fc0a5409e86
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 00:15:06 -0400

    inception

--- neuralnets/vgg.py
+++ neuralnets/vgg.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from keras.models import Model
 from keras.layers import Input
 from keras.layers import Conv2D
@@ -28,17 +30,29 @@ the inception module in the GoogLeNet, and the residual module in the ResNet.
 
 def vggblock(layer_in, n_filters, n_conv):
     """
-    Create VGG block.
+    Create VGG bloc for input layer layer_in, number of filters n_filters, and number of 
+    convolutions n_conv. 
     """
     for _ in range(n_conv):
         layer_in = Conv2D(n_filters, (3,3), padding="same", activation="relu")(layer_in)
     layer_in = MaxPooling2D((2,2), strides=(2,2))(layer_in) # max pooling layer
     return layer_in
 
+def inception_module(layer_in, f1, f2, f3):
+    """
+    Create naive inception block.
+    """
+    conv1 = Conv2D(f1, (1,1), padding="same", activation="relu")(layer_in) # 1x1 
+    conv3 = Conv2D(f2, (3,3), padding="same", activation="relu")(layer_in) # 3x3
+    conv5 = Conv2D(f3, (5,5), padding="same", activation="relu")(layer_in) # 5x5
+    pool = MaxPooling2D((3,3), strides=(1,1), padding="same")(layer_in) # Max pooling
+    layer_out = np.concatenate([conv1, conv3, conv5, pool], axis=-1)
+    return layer_out
+
 visible = Input(shape=(256, 256, 3)) # define model input
-layer = vgg_block(visible, 64, 2) # add vgg module
-layer = vgg_block(layer, 128, 2) 
-layer = vgg_block(layer, 256, 4)
+layer = vggblock(visible, 64, 2) # add vgg module
+layer = vggblock(layer, 128, 2) 
+layer = vggblock(layer, 256, 4)
 model = Model(inputs=visible, outputs=layer) # create mode
 model.summary() # summarize model
 plot_model(model, show_shapes=True, to_file="multiplevggblocks.png") # plot model architecture
