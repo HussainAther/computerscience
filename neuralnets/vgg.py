12c404c9226ea849c01bb9abe627143b0a905cac
Syed Hussain Ather <shussainather@gmail.com>
2019-05-18 00:17:11 -0400

    tie in layers

--- neuralnets/vgg.py
+++ neuralnets/vgg.py
@@ -44,8 +44,11 @@ def inception_module(layer_in, f1, f2, f3):
     """
     conv1 = Conv2D(f1, (1,1), padding="same", activation="relu")(layer_in) # 1x1 
     conv3 = Conv2D(f2, (3,3), padding="same", activation="relu")(layer_in) # 3x3
+    conv3 = Conv2D(f2_out, (3,3), padding="same", activation="relu")(conv3)
     conv5 = Conv2D(f3, (5,5), padding="same", activation="relu")(layer_in) # 5x5
+    conv5 = Conv2D(f3_out, (5,5), padding="same", activation="relu")(conv5)
     pool = MaxPooling2D((3,3), strides=(1,1), padding="same")(layer_in) # Max pooling
+    pool = Conv2D(f4_out, (1,1), padding="same", activation="relu")(pool)
     layer_out = np.concatenate([conv1, conv3, conv5, pool], axis=-1)
     return layer_out
 
