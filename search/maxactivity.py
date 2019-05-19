4cf1c473db99447ec5069bf1fea8ebb6aa43ed9b
Syed Hussain Ather <shussainather@gmail.com>
2019-05-19 00:21:01 -0400

    init commit

---
+++ search/maxactivity.py
@@ -0,0 +1,5 @@
+"""
+Assume activities are sorted according to finish time. Print a maximum set
+of activities that can be done by a single person with n total nunmber of 
+activities, s array of start times, and f array of finish times.
+"""
