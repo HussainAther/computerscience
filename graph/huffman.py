1899971560a607bbfb1a0d9b7c8f7228e9ea4300
Syed Hussain Ather <shussainather@gmail.com>
2019-05-20 14:57:48 -0400

    import

--- graph/huffman.py
+++ graph/huffman.py
@@ -1,4 +1,8 @@
+import heapq
+import os
+
 """
 Huffman coding is a lossless data compression technique. It uses variable-length
-codes to the input characters based on the frequencies of their occurence.
+codes to the input characters based on the frequencies of their occurence. The
+most frequent character has the smallest length code.
 """
