90b969031e85c36b5d782cc4fbc6b6258722df85
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 01:17:28 -0400

    return opt

--- search/lcs.py
+++ search/lcs.py
@@ -10,5 +10,20 @@ def lcslength(x, y)
     """
     m = len(x)
     n = len(y)
-    b, c = {}, {} # output tables
-
+    b, c = [[]], [[]] # output table lists
+    for i in range(m):
+        c[i][0] = 0
+    for j in range(n):
+        c[0][j] = 0
+    for i in range(1, m):
+        for j in range(1, n):
+            if x[i] == y[j]:
+                c[i][j] = c[i-1][j-1] + 1
+                b[i][j] = "upleft"
+            elif c[i-1][j] >= c[i][j-1]:
+                c[i][j] = c[i-1][j]
+                b[i][j] = "up"
+            else:
+                c[i][j] = c[i][j-1]
+                b[i][j] = "left"
+    return b, c 
