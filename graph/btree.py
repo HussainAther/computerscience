0ea05bbc493715747e26010cb916caddbf1b470d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 01:37:36 -0400

    class

--- graph/btree.py
+++ graph/btree.py
@@ -1,3 +1,10 @@
+import numbers
+
 """
 B-tree (b tree btree) implementation.
 """
+
+class BTreeSet(object):
+    """
+    The degree is the minimum number of children each non-root internal node must have.
+    """
