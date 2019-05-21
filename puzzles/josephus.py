a5ab0e3b7f181ab90e4c2a4cb405eaf27f40febe
Syed Hussain Ather <shussainather@gmail.com>
2019-05-20 21:46:08 -0400

    def

--- puzzles/josephus.py
+++ puzzles/josephus.py
@@ -8,3 +8,12 @@ people. The order in which the people are removed from the circle defines
 the (n-m)-Josephus permutation of the integers 1; 2; : : : ; n. 
 For example, the .7; 3/-Josephus permutation is h3; 6; 2; 7; 5; 1; 4i.
 """
+
+def josephus(n, k): 
+    """
+    Recursive call for removing people.
+    """ 
+    if n == 1: 
+        return 1
+    else: 
+        return (josephus(n - 1, k) + k-1) % n + 1
