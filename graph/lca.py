5bbaa0b5e2c68b1e96d7c9e40f9907f97d1910a1
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 02:23:15 -0400

    comment

--- graph/lca.py
+++ graph/lca.py
@@ -3,3 +3,8 @@ Lowest common ancestor (lca) for a given pair of nodes.
 """
 
 def lca(G, node1, node2, default=None):
+    """
+    Compute the lowest common ancestor of the given pair of nodes.
+    Only defined on non-null directed acyclic graphs.
+    Takes n log(n) time in the size of the graph.
+    """
