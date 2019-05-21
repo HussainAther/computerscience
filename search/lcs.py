0cbb292322f6de58c9169a452eccb6251eed90dc
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 01:19:07 -0400

    printlcs

--- search/lcs.py
+++ search/lcs.py
@@ -27,3 +27,17 @@ def lcslength(x, y)
                 c[i][j] = c[i][j-1]
                 b[i][j] = "left"
     return b, c 
+
+def printlcs(b, x, i, j):
+    """
+    Print the most optimal solutions.
+    """
+    if i == 0 or j == 0:
+        return 
+    elif b[i][j] == "upleft":
+        printlcs(b, x, i-1, j-1)
+        print(x[i])
+    elif b[i][j] == "up":
+        printlcs(b, x, i-1, j)
+    else:
+        printlcs(b, x, i, j-1)
