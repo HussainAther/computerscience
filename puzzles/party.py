a0651f82054dc23fe2edc454e27172e873b4f70e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-21 00:11:44 -0400

    Xcode carats

--- puzzles/party.py
+++ puzzles/party.py
@@ -8,10 +8,10 @@ means you wish to go for the hour when you get to hob-nob with the maximum numbe
 celebrities and get selfies with each of them.
 
 We are given a list of intervals that correspond to when each celebrity comes and goes.
-Assume that these intervals are initially inclusive, where the first and second values correspond to hours.
+Assume that these intervals are initially inclusive, where the first and second values correspond to hours.
 That is, the interval is closed on the left hand side and open on the right hand side.
 This just means that the celebrity will be partying on and through the th hour, but will
-have left when the th hour begins. So even if you arrive on dot on the th hour, you will
+have left when the th hour begins. So even if you arrive on dot on the th hour, you will
 miss this particular celebrity.
 
 When is the best time to attend the party? That is, which hour should you go to?
