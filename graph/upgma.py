da78a1b85fea0e2f3d8fc5dfaa757f94ba24035c
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 02:41:10 -0400

    class

--- graph/upgma.py
+++ graph/upgma.py
@@ -5,3 +5,13 @@ import matplotlib.pyplot as plt
 UPGMA (unweighted pair group method with arithmetic mean) is a simple 
 agglomerative (bottom-up) hierarchical clustering method. 
 """
+
+class Node:
+    """
+    Basic Node node class for the tree nodes.
+    """
+    def __init__(self, p):
+        self.points = p
+        self.right = None
+        self.left = None
+
