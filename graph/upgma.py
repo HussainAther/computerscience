65135b23f2f3b03bd762e1af1b50be8cbb87f154
Syed Hussain Ather <shussainather@gmail.com>
2019-05-24 02:42:23 -0400

    return dist

--- graph/upgma.py
+++ graph/upgma.py
@@ -22,3 +22,12 @@ def distmat(points):
     n = len(points)
     dim = len(points[0])
     dist = [[0 for x in xrange(n)] for y in xrange(n)]
+    for i in xrange(n):
+        for j in xrange(n):
+            d = 0
+            p1 = points[i]
+            p2 = points[j]
+            for k in xrange(dim):
+                d = d + (p1[k]-p2[k])**2
+            dist[i][j] = math.sqrt(d)
+    return dist
