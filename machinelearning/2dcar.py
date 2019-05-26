467623926a06275fee3c92cc6b6f8a42df1dcfc3
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 18:24:49 -0400

    quotes and spacing

--- machinelearning/2dcar.py
+++ machinelearning/2dcar.py
@@ -10,7 +10,6 @@ import shutil
 
 pyglet.clock.set_fps_limit(10000)
 
-
 class CarEnv(object):
     n_sensor = 5
     action_dim = 1
@@ -141,18 +140,17 @@ class CarEnv(object):
             if distance < self.car_info[-1]/2:
                 self.terminal = True
 
-
 class Viewer(pyglet.window.Window):
     color = {
-        'background': [1]*3 + [1]
+        "background": [1]*3 + [1]
     }
     fps_display = pyglet.clock.ClockDisplay()
     bar_thc = 5
 
     def __init__(self, width, height, car_info, sensor_info, obstacle_coords):
-        super(Viewer, self).__init__(width, height, resizable=False, caption='2D car', vsync=False)  # vsync=False to not use the monitor FPS
+        super(Viewer, self).__init__(width, height, resizable=False, caption="2D car", vsync=False)  # vsync=False to not use the monitor FPS
         self.set_location(x=80, y=10)
-        pyglet.gl.glClearColor(*self.color['background'])
+        pyglet.gl.glClearColor(*self.color["background"])
 
         self.car_info = car_info
         self.sensor_info = sensor_info
@@ -165,21 +163,21 @@ class Viewer(pyglet.window.Window):
         line_coord = [0, 0] * 2
         c = (73, 73, 73) * 2
         for i in range(len(self.sensor_info)):
-            self.sensors.append(self.batch.add(2, pyglet.gl.GL_LINES, foreground, ('v2f', line_coord), ('c3B', c)))
+            self.sensors.append(self.batch.add(2, pyglet.gl.GL_LINES, foreground, ("v2f", line_coord), ("c3B", c)))
 
         car_box = [0, 0] * 4
         c = (249, 86, 86) * 4
-        self.car = self.batch.add(4, pyglet.gl.GL_QUADS, foreground, ('v2f', car_box), ('c3B', c))
+        self.car = self.batch.add(4, pyglet.gl.GL_QUADS, foreground, ("v2f", car_box), ("c3B", c))
 
         c = (134, 181, 244) * 4
-        self.obstacle = self.batch.add(4, pyglet.gl.GL_QUADS, background, ('v2f', obstacle_coords.flatten()), ('c3B', c))
+        self.obstacle = self.batch.add(4, pyglet.gl.GL_QUADS, background, ("v2f", obstacle_coords.flatten()), ("c3B", c))
 
     def render(self):
         pyglet.clock.tick()
         self._update()
         self.switch_to()
         self.dispatch_events()
-        self.dispatch_event('on_draw')
+        self.dispatch_event("on_draw")
         self.flip()
 
     def on_draw(self):
@@ -237,13 +235,12 @@ ACTION_DIM = env.action_dim
 ACTION_BOUND = env.action_bound
 
 # all placeholder for tf
-with tf.name_scope('S'):
-    S = tf.placeholder(tf.float32, shape=[None, STATE_DIM], name='s')
-with tf.name_scope('R'):
-    R = tf.placeholder(tf.float32, [None, 1], name='r')
-with tf.name_scope('S_'):
-    S_ = tf.placeholder(tf.float32, shape=[None, STATE_DIM], name='s_')
-
+with tf.name_scope("S"):
+    S = tf.placeholder(tf.float32, shape=[None, STATE_DIM], name="s")
+with tf.name_scope("R"):
+    R = tf.placeholder(tf.float32, [None, 1], name="r")
+with tf.name_scope("S_"):
+    S_ = tf.placeholder(tf.float32, shape=[None, STATE_DIM], name="s_")
 
 class Actor(object):
     def __init__(self, sess, action_dim, action_bound, learning_rate, t_replace_iter):
@@ -254,30 +251,30 @@ class Actor(object):
         self.t_replace_iter = t_replace_iter
         self.t_replace_counter = 0
 
-        with tf.variable_scope('Actor'):
+        with tf.variable_scope("Actor"):
             # input s, output a
-            self.a = self._build_net(S, scope='eval_net', trainable=True)
+            self.a = self._build_net(S, scope="eval_net", trainable=True)
 
             # input s_, output a, get a_ for critic
-            self.a_ = self._build_net(S_, scope='target_net', trainable=False)
+            self.a_ = self._build_net(S_, scope="target_net", trainable=False)
 
-        self.e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/eval_net')
-        self.t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/target_net')
+        self.e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="Actor/eval_net")
+        self.t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="Actor/target_net")
 
     def _build_net(self, s, scope, trainable):
         with tf.variable_scope(scope):
             init_w = tf.contrib.layers.xavier_initializer()
             init_b = tf.constant_initializer(0.001)
             net = tf.layers.dense(s, 100, activation=tf.nn.relu,
-                                  kernel_initializer=init_w, bias_initializer=init_b, name='l1',
+                                  kernel_initializer=init_w, bias_initializer=init_b, name="l1",
                                   trainable=trainable)
             net = tf.layers.dense(net, 20, activation=tf.nn.relu,
-                                  kernel_initializer=init_w, bias_initializer=init_b, name='l2',
+                                  kernel_initializer=init_w, bias_initializer=init_b, name="l2",
                                   trainable=trainable)
-            with tf.variable_scope('a'):
+            with tf.variable_scope("a"):
                 actions = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, kernel_initializer=init_w,
-                                          name='a', trainable=trainable)
-                scaled_a = tf.multiply(actions, self.action_bound, name='scaled_a')  # Scale output to -action_bound to action_bound
+                                          name="a", trainable=trainable)
+                scaled_a = tf.multiply(actions, self.action_bound, name="scaled_a")  # Scale output to -action_bound to action_bound
         return scaled_a
 
     def learn(self, s):   # batch update
@@ -291,13 +288,12 @@ class Actor(object):
         return self.sess.run(self.a, feed_dict={S: s})[0]  # single action
 
     def add_grad_to_graph(self, a_grads):
-        with tf.variable_scope('policy_grads'):
+        with tf.variable_scope("policy_grads"):
             self.policy_grads = tf.gradients(ys=self.a, xs=self.e_params, grad_ys=a_grads)
 
-        with tf.variable_scope('A_train'):
+        with tf.variable_scope("A_train"):
             opt = tf.train.RMSPropOptimizer(-self.lr)  # (- learning rate) for ascent policy
             self.train_op = opt.apply_gradients(zip(self.policy_grads, self.e_params))
-
 
 class Critic(object):
     def __init__(self, sess, state_dim, action_dim, learning_rate, gamma, t_replace_iter, a, a_):
@@ -309,27 +305,27 @@ class Critic(object):
         self.t_replace_iter = t_replace_iter
         self.t_replace_counter = 0
 
-        with tf.variable_scope('Critic'):
+        with tf.variable_scope("Critic"):
             # Input (s, a), output q
             self.a = a
-            self.q = self._build_net(S, self.a, 'eval_net', trainable=True)
+            self.q = self._build_net(S, self.a, "eval_net", trainable=True)
 
             # Input (s_, a_), output q_ for q_target
-            self.q_ = self._build_net(S_, a_, 'target_net', trainable=False)    # target_q is based on a_ from Actor's target_net
+            self.q_ = self._build_net(S_, a_, "target_net", trainable=False)    # target_q is based on a_ from Actor"s target_net
 
-            self.e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval_net')
-            self.t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/target_net')
+            self.e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="Critic/eval_net")
+            self.t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="Critic/target_net")
 
-        with tf.variable_scope('target_q'):
+        with tf.variable_scope("target_q"):
             self.target_q = R + self.gamma * self.q_
 
-        with tf.variable_scope('TD_error'):
+        with tf.variable_scope("TD_error"):
             self.loss = tf.reduce_mean(tf.squared_difference(self.target_q, self.q))
 
-        with tf.variable_scope('C_train'):
+        with tf.variable_scope("C_train"):
             self.train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
 
-        with tf.variable_scope('a_grad'):
+        with tf.variable_scope("a_grad"):
             self.a_grads = tf.gradients(self.q, a)[0]   # tensor of gradients of each sample (None, a_dim)
 
     def _build_net(self, s, a, scope, trainable):
@@ -337,16 +333,16 @@ class Critic(object):
             init_w = tf.contrib.layers.xavier_initializer()
             init_b = tf.constant_initializer(0.01)
 
-            with tf.variable_scope('l1'):
+            with tf.variable_scope("l1"):
                 n_l1 = 100
-                w1_s = tf.get_variable('w1_s', [self.s_dim, n_l1], initializer=init_w, trainable=trainable)
-                w1_a = tf.get_variable('w1_a', [self.a_dim, n_l1], initializer=init_w, trainable=trainable)
-                b1 = tf.get_variable('b1', [1, n_l1], initializer=init_b, trainable=trainable)
+                w1_s = tf.get_variable("w1_s", [self.s_dim, n_l1], initializer=init_w, trainable=trainable)
+                w1_a = tf.get_variable("w1_a", [self.a_dim, n_l1], initializer=init_w, trainable=trainable)
+                b1 = tf.get_variable("b1", [1, n_l1], initializer=init_b, trainable=trainable)
                 net = tf.nn.relu6(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
             net = tf.layers.dense(net, 20, activation=tf.nn.relu,
-                                  kernel_initializer=init_w, bias_initializer=init_b, name='l2',
+                                  kernel_initializer=init_w, bias_initializer=init_b, name="l2",
                                   trainable=trainable)
-            with tf.variable_scope('q'):
+            with tf.variable_scope("q"):
                 q = tf.layers.dense(net, 1, kernel_initializer=init_w, bias_initializer=init_b, trainable=trainable)   # Q(s,a)
         return q
 
@@ -355,7 +351,6 @@ class Critic(object):
         if self.t_replace_counter % self.t_replace_iter == 0:
             self.sess.run([tf.assign(t, e) for t, e in zip(self.t_params, self.e_params)])
         self.t_replace_counter += 1
-
 
 class Memory(object):
     def __init__(self, capacity, dims):
@@ -370,10 +365,9 @@ class Memory(object):
         self.pointer += 1
 
     def sample(self, n):
-        assert self.pointer >= self.capacity, 'Memory has not been fulfilled'
+        assert self.pointer >= self.capacity, "Memory has not been fulfilled"
         indices = np.random.choice(self.capacity, size=n)
         return self.data[indices, :]
-
 
 sess = tf.Session()
 
@@ -385,13 +379,12 @@ actor.add_grad_to_graph(critic.a_grads)
 M = Memory(MEMORY_CAPACITY, dims=2 * STATE_DIM + ACTION_DIM + 1)
 
 saver = tf.train.Saver()
-path = './discrete' if DISCRETE_ACTION else './continuous'
+path = "./discrete" if DISCRETE_ACTION else "./continuous"
 
 if LOAD:
     saver.restore(sess, tf.train.latest_checkpoint(path))
 else:
     sess.run(tf.global_variables_initializer())
-
 
 def train():
     var = 2.  # control exploration
@@ -426,18 +419,17 @@ def train():
 
             if done or t == MAX_EP_STEPS - 1:
             # if done:
-                print('Ep:', ep,
-                      '| Steps: %i' % int(ep_step),
-                      '| Explore: %.2f' % var,
+                print("Ep:", ep,
+                      "| Steps: %i" % int(ep_step),
+                      "| Explore: %.2f" % var,
                       )
                 break
 
     if os.path.isdir(path): shutil.rmtree(path)
     os.mkdir(path)
-    ckpt_path = os.path.join(path, 'DDPG.ckpt')
+    ckpt_path = os.path.join(path, "DDPG.ckpt")
     save_path = saver.save(sess, ckpt_path, write_meta_graph=False)
     print("\nSave Model %s\n" % save_path)
-
 
 def eval():
     env.set_fps(30)
@@ -464,20 +456,19 @@ def intersection():
         t1 = t0 + np.dot(s, r)/np.dot(r, r)
         print(t1, t0)
         if ((np.dot(s, r) > 0) and (0 <= t1 - t0 <= 1)) or ((np.dot(s, r) <= 0) and (0 <= t0 - t1 <= 1)):
-            print('collinear and overlapping, q_s in p_r')
+            print("collinear and overlapping, q_s in p_r")
         else:
-            print('collinear and disjoint')
+            print("collinear and disjoint")
     elif np.cross(r, s) == 0 and np.cross((q-p), r) != 0:  # parallel r × s = 0 and (q − p) × r ≠ 0,
-        print('parallel')
+        print("parallel")
     else:
         t = np.cross((q - p), s) / np.cross(r, s)
         u = np.cross((q - p), r) / np.cross(r, s)
         if 0 <= t <= 1 and 0 <= u <= 1:
             # If r × s ≠ 0 and 0 ≤ t ≤ 1 and 0 ≤ u ≤ 1, the two line segments meet at the point p + t r = q + u s
-            print('intersection: ', p + t*r)
+            print("intersection: ", p + t*r)
         else:
-            print('not parallel and not intersect')
-
+            print("not parallel and not intersect")
 
 def point2segment():
     p = np.array([-1, 1])    # coordination of point
@@ -495,11 +486,11 @@ def point2segment():
     cosTheta2 = np.dot(bp, ab) / (np.linalg.norm(bp) * np.linalg.norm(ab))
     theta2 = np.arccos(cosTheta2)
     if np.pi/2 <= (theta1 % (np.pi*2)) <= 3/2 * np.pi:
-        print('out of a')
+        print("out of a")
     elif -np.pi/2 <= (theta2 % (np.pi*2)) <= np.pi/2:
-        print('out of b')
+        print("out of b")
     else:
-        print('between a and b')
+        print("between a and b")
 
 np.random.seed(1)
 env = CarEnv()
