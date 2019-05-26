2f7ae4ff8bbdb59dc856be68a80e7ba01367a158
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 18:42:20 -0400

    plot

--- machinelearning/robotarm.py
+++ machinelearning/robotarm.py
@@ -374,6 +374,72 @@ def eval():
         s_, r, done = env.step(a)
         s = s_
 
+class PPO(object):
+    def __init__(self):
+        self.sess = tf.Session()
+
+        self.tfs = tf.placeholder(tf.float32, [None, S_DIM], 'state')
+
+        # critic
+        l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu)
+        self.v = tf.layers.dense(l1, 1)
+        self.tfdc_r = tf.placeholder(tf.float32, [None, 1], 'discounted_r')
+        self.advantage = self.tfdc_r - self.v
+        self.closs = tf.reduce_mean(tf.square(self.advantage))
+        self.ctrain_op = tf.train.AdamOptimizer(C_LR).minimize(self.closs)
+
+        # actor
+        pi, pi_params = self._build_anet('pi', trainable=True)
+        oldpi, oldpi_params = self._build_anet('oldpi', trainable=False)
+        self.sample_op = tf.squeeze(pi.sample(1), axis=0)  # choosing action
+        self.update_oldpi_op = [oldp.assign(p) for p, oldp in zip(pi_params, oldpi_params)]
+
+        self.tfa = tf.placeholder(tf.float32, [None, A_DIM], 'action')
+        self.tfadv = tf.placeholder(tf.float32, [None, 1], 'advantage')
+        # ratio = tf.exp(pi.log_prob(self.tfa) - oldpi.log_prob(self.tfa))
+        ratio = pi.prob(self.tfa) / (oldpi.prob(self.tfa) + 1e-5)
+        surr = ratio * self.tfadv   # surrogate loss
+
+        self.aloss = -tf.reduce_mean(tf.minimum(
+            surr,
+            tf.clip_by_value(ratio, 1. - EPSILON, 1. + EPSILON) * self.tfadv))
+
+        self.atrain_op = tf.train.AdamOptimizer(A_LR).minimize(self.aloss)
+        self.sess.run(tf.global_variables_initializer())
+
+    def update(self):
+        global GLOBAL_UPDATE_COUNTER
+        while not COORD.should_stop():
+            if GLOBAL_EP < EP_MAX:
+                UPDATE_EVENT.wait()         # wait until get batch of data
+                self.sess.run(self.update_oldpi_op)   # old pi to pi
+                data = [QUEUE.get() for _ in range(QUEUE.qsize())]
+                data = np.vstack(data)
+                s, a, r = data[:, :S_DIM], data[:, S_DIM: S_DIM + A_DIM], data[:, -1:]
+                adv = self.sess.run(self.advantage, {self.tfs: s, self.tfdc_r: r})
+                [self.sess.run(self.atrain_op, {self.tfs: s, self.tfa: a, self.tfadv: adv}) for _ in range(UPDATE_STEP)]
+                [self.sess.run(self.ctrain_op, {self.tfs: s, self.tfdc_r: r}) for _ in range(UPDATE_STEP)]
+                UPDATE_EVENT.clear()        # updating finished
+                GLOBAL_UPDATE_COUNTER = 0   # reset counter
+                ROLLING_EVENT.set()         # set roll-out available
+
+    def _build_anet(self, name, trainable):
+        with tf.variable_scope(name):
+            l1 = tf.layers.dense(self.tfs, 200, tf.nn.relu, trainable=trainable)
+            mu = A_BOUND * tf.layers.dense(l1, A_DIM, tf.nn.tanh, trainable=trainable)
+            sigma = tf.layers.dense(l1, A_DIM, tf.nn.softplus, trainable=trainable)
+            norm_dist = Normal(loc=mu, scale=sigma)
+        params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
+        return norm_dist, params
+
+    def choose_action(self, s):
+        s = s[np.newaxis, :]
+        a = self.sess.run(self.sample_op, {self.tfs: s})[0]
+        return np.clip(a, -2, 2)
+
+    def get_v(self, s):
+        if s.ndim < 2: s = s[np.newaxis, :]
+        return self.sess.run(self.v, {self.tfs: s})[0, 0]
 
 COORD = tf.train.Coordinator()
 SESS.run(tf.global_variables_initializer())
@@ -400,3 +466,33 @@ COORD.join(worker_threads)
 
 train()
 eval()
+
+GLOBAL_PPO = PPO()
+UPDATE_EVENT, ROLLING_EVENT = threading.Event(), threading.Event()
+UPDATE_EVENT.clear()    # no update now
+ROLLING_EVENT.set()     # start to roll out
+workers = [Worker(wid=i) for i in range(N_WORKER)]
+
+GLOBAL_UPDATE_COUNTER, GLOBAL_EP = 0, 0
+GLOBAL_RUNNING_R = []
+COORD = tf.train.Coordinator()
+QUEUE = queue.Queue()
+threads = []
+for worker in workers:  # worker threads
+    t = threading.Thread(target=worker.work, args=())
+    t.start()
+    threads.append(t)
+# add a PPO updating thread
+threads.append(threading.Thread(target=GLOBAL_PPO.update,))
+threads[-1].start()
+COORD.join(threads)
+
+# plot reward change and testing
+plt.plot(np.arange(len(GLOBAL_RUNNING_R)), GLOBAL_RUNNING_R)
+plt.xlabel('Episode'); plt.ylabel('Moving reward'); plt.ion(); plt.show()
+env.set_fps(30)
+while True:
+    s = env.reset()
+    for t in range(400):
+        env.render()
+        s = env.step(GLOBAL_PPO.choose_action(s))[0]
