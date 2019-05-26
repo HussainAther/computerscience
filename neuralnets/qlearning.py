65a301c0454bec5e1ff5e402dd558f9b77b40c5e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 15:31:28 -0400

    return

--- neuralnets/qlearning.py
+++ neuralnets/qlearning.py
@@ -19,17 +19,29 @@ env = WindyGridworldEnv()
 """
 The Q in Q-learning represents the expected utility (Quality) of a state-action 
 pairing. Q-learning models learn about state-action pairs, such that for 
-any given state, multiple possible actions may be entertained. (q learning qlearning Qlearning). 
+any given state, multiple possible actions may be entertained. (q learning qlearning Qlearning). Q-learning is a type of reinforcement algorithm for learning. 
 """
 
 def createEpsilonGreedyPolicy(Q, epsilon, num_actions): 
     """ 
     Creates an epsilon-greedy policy based 
-    on a given Q-function and epsilon. 
+    on a given Q-function and epsilon for a number of 
+    actions num_actions. 
        
     Returns a function that takes the state 
     as an input and returns the probabilities 
     for each action in the form of a numpy array  
     of length of the action space(set of possible actions). 
     """
-    def policyFunction(state): 
+    def policyFunction(state):
+        """
+        Select a function based on the state for this policy.
+        """
+        Action_probabilities = np.ones(num_actions, 
+                dtype = float) * epsilon / num_actions 
+                  
+        best_action = np.argmax(Q[state]) 
+        Action_probabilities[best_action] += (1.0 - epsilon) 
+        return Action_probabilities 
+   
+    return policyFunction  
