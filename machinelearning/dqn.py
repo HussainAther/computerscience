a1f03b14ce79344aa988e6566fb82e8862bf018f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 18:18:50 -0400

    quotes

--- machinelearning/dqn.py
+++ machinelearning/dqn.py
@@ -16,12 +16,12 @@ MEMORY_SIZE = 3000
 ACTION_SPACE = 25
 
 sess = tf.Session()
-with tf.variable_scope('natural'):
+with tf.variable_scope("natural"):
     natural_DQN = DuelingDQN(
         n_actions=ACTION_SPACE, n_features=3, memory_size=MEMORY_SIZE,
         e_greedy_increment=0.001, sess=sess, dueling=False)
 
-with tf.variable_scope('dueling'):
+with tf.variable_scope("dueling"):
     dueling_DQN = DuelingDQN(
         n_actions=ACTION_SPACE, n_features=3, memory_size=MEMORY_SIZE,
         e_greedy_increment=0.001, sess=sess, dueling=True, output_graph=True)
@@ -60,8 +60,8 @@ class DuelingDQN:
         self.learn_step_counter = 0
         self.memory = np.zeros((self.memory_size, n_features*2+2))
         self._build_net()
-        t_params = tf.get_collection('target_net_params')
-        e_params = tf.get_collection('eval_net_params')
+        t_params = tf.get_collection("target_net_params")
+        e_params = tf.get_collection("eval_net_params")
         self.replace_target_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
 
         if sess is None:
@@ -75,57 +75,57 @@ class DuelingDQN:
 
     def _build_net(self):
         def build_layers(s, c_names, n_l1, w_initializer, b_initializer):
-            with tf.variable_scope('l1'):
-                w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
-                b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
+            with tf.variable_scope("l1"):
+                w1 = tf.get_variable("w1", [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
+                b1 = tf.get_variable("b1", [1, n_l1], initializer=b_initializer, collections=c_names)
                 l1 = tf.nn.relu(tf.matmul(s, w1) + b1)
 
             if self.dueling:
                 # Dueling DQN
-                with tf.variable_scope('Value'):
-                    w2 = tf.get_variable('w2', [n_l1, 1], initializer=w_initializer, collections=c_names)
-                    b2 = tf.get_variable('b2', [1, 1], initializer=b_initializer, collections=c_names)
+                with tf.variable_scope("Value"):
+                    w2 = tf.get_variable("w2", [n_l1, 1], initializer=w_initializer, collections=c_names)
+                    b2 = tf.get_variable("b2", [1, 1], initializer=b_initializer, collections=c_names)
                     self.V = tf.matmul(l1, w2) + b2
 
-                with tf.variable_scope('Advantage'):
-                    w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
-                    b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
+                with tf.variable_scope("Advantage"):
+                    w2 = tf.get_variable("w2", [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
+                    b2 = tf.get_variable("b2", [1, self.n_actions], initializer=b_initializer, collections=c_names)
                     self.A = tf.matmul(l1, w2) + b2
 
-                with tf.variable_scope('Q'):
+                with tf.variable_scope("Q"):
                     out = self.V + (self.A - tf.reduce_mean(self.A, axis=1, keep_dims=True))     # Q = V(s) + A(s,a)
             else:
-                with tf.variable_scope('Q'):
-                    w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
-                    b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
+                with tf.variable_scope("Q"):
+                    w2 = tf.get_variable("w2", [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
+                    b2 = tf.get_variable("b2", [1, self.n_actions], initializer=b_initializer, collections=c_names)
                     out = tf.matmul(l1, w2) + b2
 
             return out
 
         # ------------------ build evaluate_net ------------------
-        self.s = tf.placeholder(tf.float32, [None, self.n_features], name='s')  # input
-        self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='Q_target')  # for calculating loss
-        with tf.variable_scope('eval_net'):
+        self.s = tf.placeholder(tf.float32, [None, self.n_features], name="s")  # input
+        self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name="Q_target")  # for calculating loss
+        with tf.variable_scope("eval_net"):
             c_names, n_l1, w_initializer, b_initializer = \
-                ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES], 20, \
+                ["eval_net_params", tf.GraphKeys.GLOBAL_VARIABLES], 20, \
                 tf.random_normal_initializer(0., 0.3), tf.constant_initializer(0.1)  # config of layers
 
             self.q_eval = build_layers(self.s, c_names, n_l1, w_initializer, b_initializer)
 
-        with tf.variable_scope('loss'):
+        with tf.variable_scope("loss"):
             self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
-        with tf.variable_scope('train'):
+        with tf.variable_scope("train"):
             self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
 
         # ------------------ build target_net ------------------
-        self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name='s_')    # input
-        with tf.variable_scope('target_net'):
-            c_names = ['target_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
+        self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name="s_")    # input
+        with tf.variable_scope("target_net"):
+            c_names = ["target_net_params", tf.GraphKeys.GLOBAL_VARIABLES]
 
             self.q_next = build_layers(self.s_, c_names, n_l1, w_initializer, b_initializer)
 
     def store_transition(self, s, a, r, s_):
-        if not hasattr(self, 'memory_counter'):
+        if not hasattr(self, "memory_counter"):
             self.memory_counter = 0
         transition = np.hstack((s, [a, r], s_))
         index = self.memory_counter % self.memory_size
@@ -144,7 +144,7 @@ class DuelingDQN:
     def learn(self):
         if self.learn_step_counter % self.replace_target_iter == 0:
             self.sess.run(self.replace_target_op)
-            print('\ntarget_params_replaced\n')
+            print("\ntarget_params_replaced\n")
 
         sample_index = np.random.choice(self.memory_size, size=self.batch_size)
         batch_memory = self.memory[sample_index, :]
@@ -199,19 +199,19 @@ c_natural, r_natural = train(natural_DQN)
 c_dueling, r_dueling = train(dueling_DQN)
 
 plt.figure(1)
-plt.plot(np.array(c_natural), c='r', label='natural')
-plt.plot(np.array(c_dueling), c='b', label='dueling')
-plt.legend(loc='best')
-plt.ylabel('cost')
-plt.xlabel('training steps')
+plt.plot(np.array(c_natural), c="r", label="natural")
+plt.plot(np.array(c_dueling), c="b", label="dueling")
+plt.legend(loc="best")
+plt.ylabel("cost")
+plt.xlabel("training steps")
 plt.grid()
 
 plt.figure(2)
-plt.plot(np.array(r_natural), c='r', label='natural')
-plt.plot(np.array(r_dueling), c='b', label='dueling')
-plt.legend(loc='best')
-plt.ylabel('accumulated reward')
-plt.xlabel('training steps')
+plt.plot(np.array(r_natural), c="r", label="natural")
+plt.plot(np.array(r_dueling), c="b", label="dueling")
+plt.legend(loc="best")
+plt.ylabel("accumulated reward")
+plt.xlabel("training steps")
 plt.grid()
 
 plt.show()
