428996ad3a6a3a1094232550c8f2a0ea334c491f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 01:57:34 -0400

    init commit

---
+++ games/reversegam.py
@@ -0,0 +1,9 @@
+
+"""
+Reversegam, also known as Reversi or Othello.
+Reversegam has an 8×8 board and tiles that are black on one side and white on the other (our game will use Os and Xs instead).
+Two players take turns placing tiles of their chosen color—black or white—on the board. When a player places a tile on the board,
+any of the opponent’s tiles that are between the new tile and the other tiles of the player’s color are flipped. 
+
+
+"""
