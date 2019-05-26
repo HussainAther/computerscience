a79ca4baf3cf2297900d6529899ca9f257a7b20b
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 18:28:57 -0400

    class curiosity

--- machinelearning/curiosity.py
+++ machinelearning/curiosity.py
@@ -6,3 +6,25 @@ import matplotlib.pyplot as plt
 """
 Curiosity-driven learning (curiosity) implementation. 
 """
+
+class CuriosityNet:
+    def __init__(
+            self,
+            n_a,
+            n_s,
+            lr=0.01,
+            gamma=0.98,
+            epsilon=0.95,
+            replace_target_iter=300,
+            memory_size=10000,
+            batch_size=128,
+            output_graph=False,
+    ):
+        self.n_a = n_a
+        self.n_s = n_s
+        self.lr = lr
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.replace_target_iter = replace_target_iter
+        self.memory_size = memory_size
+        self.batch_size = batch_size
