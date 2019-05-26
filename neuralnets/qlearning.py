b802afc05cfb6fc3e379e5aff64b83d897d54828
Syed Hussain Ather <shussainather@gmail.com>
2019-05-26 15:35:10 -0400

    more loop

--- neuralnets/qlearning.py
+++ neuralnets/qlearning.py
@@ -71,4 +71,29 @@ def qLearning(env, num_episodes, discount_factor = 1.0, alpha = 0.6, epsilon = 0
         # Reset the environment and pick the first action 
         state = env.reset() 
            
-        for t in itertools.count():  
+        for t in itertools.count(): 
+            # get probabilities of all actions from current state 
+            action_probabilities = policy(state) 
+   
+            # choose action according to  
+            # the probability distribution 
+            action = np.random.choice(np.arange( 
+                      len(action_probabilities)), 
+                       p = action_probabilities) 
+   
+            # take action and get reward, transit to next state 
+            next_state, reward, done, _ = env.step(action) 
+   
+            # Update statistics 
+            stats.episode_rewards[i_episode] += reward 
+            stats.episode_lengths[i_episode] = t 
+               
+            # TD Update 
+            best_next_action = np.argmax(Q[next_state])     
+            td_target = reward + discount_factor * Q[next_state][best_next_action] 
+            td_delta = td_target - Q[state][action] 
+            Q[state][action] += alpha * td_delta 
+   
+            # done is True if episode terminated    
+            if done: 
+                break 
