118cae68f978b3dbd192016b2f30f6dbc1f6711d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-27 00:16:09 -0400

    begin gui

--- games/colorgame.py
+++ games/colorgame.py
@@ -59,7 +59,7 @@ def nextColor():
 
 def countdown(): 
     """
-    It's the final countdown.
+    It"s the final countdown.
     """
     global timeleft 
   
@@ -76,3 +76,47 @@ def countdown():
         # run the function again after 1 second. 
         timeLabel.after(1000, countdown) 
 
+# create a GUI window 
+root = tkinter.Tk() 
+  
+# set the title 
+root.title("COLORGAME") 
+  
+# set the size 
+root.geometry("375x200") 
+  
+# add an instructions label 
+instructions = tkinter.Label(root, text = "Type in the colour"
+                        "of the words, and not the word text!", 
+                                      font = ("Helvetica", 12)) 
+instructions.pack()  
+  
+# add a score label 
+scoreLabel = tkinter.Label(root, text = "Press enter to start", 
+                                      font = ("Helvetica", 12)) 
+scoreLabel.pack() 
+  
+# add a time left label 
+timeLabel = tkinter.Label(root, text = "Time left: " +
+              str(timeleft), font = ("Helvetica", 12)) 
+                
+timeLabel.pack() 
+  
+# add a label for displaying the colours 
+label = tkinter.Label(root, font = ("Helvetica", 60)) 
+label.pack() 
+  
+# add a text entry box for 
+# typing in colours 
+e = tkinter.Entry(root) 
+  
+# run the "startGame" function  
+# when the enter key is pressed 
+root.bind("<Return>", startGame) 
+e.pack() 
+  
+# set focus on the entry box 
+e.focus_set() 
+  
+# start the GUI 
+root.mainloop() 
