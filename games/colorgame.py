cab3accbc6a5b24b0a4b176d2af644e4f2e8c19c
Syed Hussain Ather <shussainather@gmail.com>
2019-05-27 00:11:54 -0400

    import

--- games/colorgame.py
+++ games/colorgame.py
@@ -1,3 +1,6 @@
+import tkinter
+import random
+
 """
 In this game player has to enter color of the word that appears on the screen and hence the score increases 
 by one, the total time to play this game is 30 seconds. Colors used in this game are Red, Blue, Green, Pink, 
