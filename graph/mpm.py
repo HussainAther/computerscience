ace888c0c0949cf4bd7f8e48758a7f3cb6c35dcb
Syed Hussain Ather <shussainather@gmail.com>
2019-05-27 01:59:23 -0400

    def

--- graph/mpm.py
+++ graph/mpm.py
@@ -15,3 +15,34 @@ update flows in the main network
 
 FORMAT = "%(asctime)-15s %(levelname)s - %(message)s"
 
+def _to_str(network):
+    """
+    Return the network in string format.
+    """
+    out = StringIO.StringIO()
+    pp = pprint.PrettyPrinter(indent=1, stream=out)
+    pp.pprint(network)
+    to_return = out.getvalue()
+    out.close()
+    return to_return
+    
+
+def read_network(f=sys.stdin):
+    """
+    Read the current netwrok as it is.
+    """
+    N = {}
+    lines = f.readlines()
+    for line in lines:
+        s = line.split(' ')
+        if len(s) == 4:
+            u, v, c, f = [int(e) for e in s]
+        else:
+            u, v, c = [int(e) for e in s]
+            f = 0
+        if u not in N:
+            N[u] = {}
+        if v not in N:
+            N[v] = {}
+        N[u][v] = {'cap': c, 'flow': f}
+    return N
