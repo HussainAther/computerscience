97111dd4932543d46804016d41b856cb79f9fc06
Syed Hussain Ather <shussainather@gmail.com>
2019-05-27 02:07:57 -0400

    bg

--- graph/mpm.py
+++ graph/mpm.py
@@ -6,7 +6,8 @@ import cStringIO as StringIO
 from collections import deque
 
 """
-Malhotra, Pramodh Kumar, and Maheshwari (MPM) algorithm.
+Malhotra, Pramodh Kumar, and Maheshwari (MPM) algorithm for solving the maximum
+flow problem. Similar to Dinic's algorithm. Uses a layered network.
 The algorithm has time complexity of (n^3). The algorithm operates in stages.
 We construct the network N(f) [residual network] and from it we find the
 auxiliary[layered] network AN(f). Then we find a maximal flow g in AN(f) and 
