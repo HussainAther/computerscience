6c31bbd2e416c761ceb62cf2f16042f79ef178fb
Syed Hussain Ather <shussainather@gmail.com>
2019-05-27 00:04:42 -0400

    init

--- machinelearning/neuralnetv3.py
+++ machinelearning/neuralnetv3.py
@@ -3,3 +3,11 @@ import numpy as np
 """
 Simplified neural network (NN) of a single neuron neural network.
 """
+
+class NeuralNetwork():
+    """
+    Class for NN functions and parameters.
+    """
+    def __init__(self):
+        np.random.seed(1)
+        self.weight_matrix = 2 * np.random.random((3,1))-1 # 3 x 1 weight matrix
