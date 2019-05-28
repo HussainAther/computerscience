7fd2a396efe1b3a4ea5c3e18bcab38676f0552b3
Syed Hussain Ather <shussainather@gmail.com>
2019-05-28 11:45:37 -0400

    Deep import

--- machinelearning/boltzmann.py
+++ machinelearning/boltzmann.py
@@ -58,3 +58,19 @@ class RBM(object):
         Calculate state using probability.
         """
         return tf.floor(probability + tf.random_uniform(tf.shape(probability), 0, 1))
+
+"""
+Deep Boltzmann machine (dbm) using scikit-learn.
+"""
+
+import numpy as np
+import argparse
+import time
+import cv2
+
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import BernoulliRBM
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
