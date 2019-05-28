7bb7a30a1c280811f9311c4d502d9c6a5ffd72d3
Syed Hussain Ather <shussainather@gmail.com>
2019-05-28 09:51:58 -0400

    import and class

--- machinelearning/boltzmann.py
+++ machinelearning/boltzmann.py
@@ -1,3 +1,10 @@
+import tensorflow as tf
+
 """
 Restricted Boltzmann machine (RBM).
 """
+
+class RBM(object):
+    def __init__(self, visible_dim, hidden_dim, learning_rate, number_of_iterations):
+        
+        self._graph = tf.Graph()
