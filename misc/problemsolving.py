4cc9a4f8e629713ce50bba1a7c0ae2936b5b81c3
Syed Hussain Ather <shussainather@gmail.com>
2019-05-28 08:58:30 -0400

    class

--- misc/problemsolving.py
+++ misc/problemsolving.py
@@ -5,3 +5,13 @@ import sys
 """
 Problem solving functionality.
 """
+
+class World:
+    """
+    Create a world we can achieve.
+    """
+    def __init__(self):
+        self.state = dict()
+        self.goals = set()
+        self.known_literals = set()
+        self.actions = dict()
