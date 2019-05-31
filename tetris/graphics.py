e1d1d3f5e1332845de7ac1a276f1a42213078cd0
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 22:16:52 -0400

    workin thru

--- tetris/graphics.py
+++ tetris/graphics.py
@@ -1,138 +1,11 @@
 # graphics.py
-"""Simple object oriented graphics library
-
-The library is designed to make it very easy for novice programmers to
-experiment with computer graphics in an object oriented fashion. It is
-written by John Zelle for use with the book "Python Programming: An
-Introduction to Computer Science" (Franklin, Beedle & Associates).
-
-LICENSE: This is open-source software released under the terms of the
-GPL (http://www.gnu.org/licenses/gpl.html).
-
-PLATFORMS: The package is a wrapper around Tkinter and should run on
-any platform where Tkinter is available.
-
-INSTALLATION: Put this file somewhere where Python can see it.
-
-OVERVIEW: There are two kinds of objects in the library. The CanvasFrame
-class implements a window where drawing can be done and various
-GraphicsObjects are provided that can be drawn into a CanvasFrame. As a
-simple example, here is a complete program to draw a circle of radius
-10 centered in a 100x100 window:
-
---------------------------------------------------------------------
-from graphics import *
-
-def main():
-    win = CanvasFrame("My Circle", 100, 100)
-    c = Circle(Point(50,50), 10)
-    c.draw(win)
-    win.getMouse() // Pause to view result
-
-main()
---------------------------------------------------------------------
-CanvasFrame objects support coordinate transformation through the
-setCoords method and pointer-based input through getMouse.
-
-The library provides the following graphical objects:
-    Point
-    Line
-    Circle
-    Oval
-    Rectangle
-    Polygon
-    Text
-    Entry (for text-based input)
-    Image
-
-Various attributes of graphical objects can be set such as
-outline-color, fill-color and line-width. Graphical objects also
-support moving and hiding for animation effects.
-
-The library also provides a very simple class for pixel-based image
-manipulation, Pixmap. A pixmap can be loaded from a file and displayed
-using an Image object. Both getPixel and setPixel methods are provided
-for manipulating the image.
-
-DOCUMENTATION: For complete documentation, see Chapter 5 of "Python
-Programming: An Introduction to Computer Science" by John Zelle,
-published by Franklin, Beedle & Associates.  Also see
-http://mcsp.wartburg.edu/zelle/python for a quick reference"""
-
-# Version 3.5 5/10/09
-# Removed all the threading crap and cleaned up the _root stuff
-#
-# Version 3.4 10/16/07
-#     Fixed GraphicsError to avoid "exploded" error messages.
-# Version 3.3 8/8/06
-#     Added checkMouse method to CanvasFrame
-# Version 3.2.2 5/30/05
-#     Cleaned up handling of exceptions in Tk thread. The graphics package
-#     now raises an exception if attempt is made to communicate with
-#     a dead Tk thread.
-# Version 3.2.1 5/22/05
-#     Added shutdown function for tk thread to eliminate race-condition
-#        error "chatter" when main thread terminates
-#     Renamed various private globals with _
-# Version 3.2 5/4/05
-#     Added Pixmap object for simple image manipulation.
-# Version 3.1 4/13/05
-#     Improved the Tk thread communication so that most Tk calls
-#        do not have to wait for synchonization with the Tk thread.
-#        (see _tkCall and _tkExec)
-# Version 3.0 12/30/04
-#     Implemented Tk event loop in separate thread. Should now work
-#        interactively with IDLE. Undocumented autoflush feature is
-#        no longer necessary. Its default is now False (off). It may
-#        be removed in a future version.
-#     Better handling of errors regarding operations on windows that
-#       have been closed.
-#     Addition of an isClosed method to CanvasFramedow class.
-
-# Version 2.2 8/26/04
-#     Fixed cloning bug reported by Joseph Oldham.
-#     Now implements deep copy of config info.
-# Version 2.1 1/15/04
-#     Added autoflush option to CanvasFrame. When True (default) updates on
-#        the window are done after each action. This makes some graphics
-#        intensive programs sluggish. Turning off autoflush causes updates
-#        to happen during idle periods or when flush is called.
-# Version 2.0
-#     Updated Documentation
-#     Made Polygon accept a list of Points in constructor
-#     Made all drawing functions call TK update for easier animations
-#          and to make the overall package work better with
-#          Python 2.3 and IDLE 1.0 under Windows (still some issues).
-#     Removed vestigial turtle graphics.
-#     Added ability to configure font for Entry objects (analogous to Text)
-#     Added setTextColor for Text as an alias of setFill
-#     Changed to class-style exceptions
-#     Fixed cloning of Text objects
-
-# Version 1.6
-#     Fixed Entry so StringVar uses _root as master, solves weird
-#            interaction with shell in Idle
-#     Fixed bug in setCoords. X and Y coordinates can increase in
-#           "non-intuitive" direction.
-#     Tweaked wm_protocol so window is not resizable and kill box closes.
-
-# Version 1.5
-#     Fixed bug in Entry. Can now define entry before creating a
-#     CanvasFrame. All CanvasFrames are now toplevel windows and share
-#     a fixed root (called _root).
-
-# Version 1.4
-#     Fixed Garbage collection of Tkinter images bug.
-#     Added ability to set text atttributes.
-#     Added Entry boxes.
+"""
+Simple object oriented graphics library
+"""
 
 import time, os, sys
 import Tkinter
 tk = Tkinter
-
-
-##########################################################################
-# Module Exceptions
 
 import exceptions
 
@@ -146,11 +19,6 @@ class GraphicsError(exceptions.Exception):
 OBJ_ALREADY_DRAWN = "Object currently drawn"
 UNSUPPORTED_METHOD = "Object doesn't support operation"
 BAD_OPTION = "Illegal option value"
-
-
-
-############################################################################
-# Graphics classes start here
 
 class CanvasFrame(tk.Frame):
 
@@ -281,9 +149,9 @@ class CanvasFrame(tk.Frame):
 
 
 class Transform:
-
-    """Internal class for 2-D coordinate transformations"""
-
+    """
+    Internal class for 2-D coordinate transformations.
+    """
     def __init__(self, w, h, xlow, ylow, xhigh, yhigh):
         # w, h are width and height of window
         # (xlow,ylow) coordinates of lower-left [raw (0,h-1)]
@@ -319,8 +187,9 @@ DEFAULT_CONFIG = {"fill":"",
                   "font": ("helvetica", 12, "normal")}
 
 class GraphicsObject:
-
-    """Generic base class for all of the drawable objects"""
+    """
+    Generic base class for all of the drawable objects.
+    """
     # A subclass of GraphicsObject should override _draw and
     #   and _move methods.
 
@@ -376,10 +245,10 @@ class GraphicsObject:
         self.id = None
 
     def move(self, dx, dy):
-
-        """move object dx units in x direction and dy units in y
-        direction"""
-
+        """
+        Move object dx units in x direction and dy units in y
+        direction.
+        """
         self._move(dx,dy)
         canvas_frame = self.canvas_frame
         if canvas_frame and not canvas_frame.isClosed():
@@ -393,9 +262,11 @@ class GraphicsObject:
             self.canvas_frame.canvas.move(self.id, x, y)
 
     def _reconfig(self, option, setting):
-        # Internal method for changing configuration of the object
-        # Raises an error if the option does not exist in the config
-        #    dictionary for this object
+        """
+        Internal method for changing configuration of the object
+        Raise an error if the option does not exist in the config
+        dictionary for this object.
+        """
         if not self.config.has_key(option):
             raise GraphicsError, UNSUPPORTED_METHOD
         options = self.config
