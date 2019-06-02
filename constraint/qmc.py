4aa72303a97737c127d4c537aa50766709713e01
Syed Hussain Ather <shussainather@gmail.com>
2019-06-02 09:12:30 -0400

    selec

--- constraint/qmc.py
+++ constraint/qmc.py
@@ -28,7 +28,7 @@ def check(binary):
         temp = []
         for i in range(len(binary)):
 	    for j in range(i+1, len(binary)):
-	        k = compare_string(binary[i], binary[j])
+	        k = comp(binary[i], binary[j])
 		if k != -1:
 		    check1[i] = "*"
 		    check1[j] = "*"
@@ -69,3 +69,39 @@ def is_for_table(string1, string2, count):
     else:
         return False 
 
+def selection(chart, prime_implicants):
+    """
+    Select implicants from the chart. 
+    """
+    temp = []
+    select = [0]*len(chart)
+    for i in range(len(chart[0])):
+        count = 0
+	rem = -1
+	for j in range(len(chart)):
+	    if chart[j][i] == 1:
+	        count += 1
+		rem = j
+	    if count == 1:
+		select[rem] = 1
+	for i in range(len(select)):
+            if select[i] == 1:
+	        for j in range(len(chart[0])):
+                    if chart[i][j] == 1:
+		        for k in range(len(chart)):
+			    chart[k][j] = 0 
+		temp.append(prime_implicants[i])
+    while 1:
+        max_n = 0; rem = -1; count_n = 0
+	for i in range(len(chart)):
+            count_n = chart[i].count(1)
+	    if count_n > max_n:
+	        max_n = count_n
+	        rem = i
+        if max_n == 0:
+            return temp
+	temp.append(prime_implicants[rem])
+        for i in range(len(chart[0])):
+            if chart[rem][i] == 1:
+	        for j in range(len(chart)):
+		    chart[j][i] = 0
