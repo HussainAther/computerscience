4cb9e6062c4acff2175cc0209ecada2935773d01
Syed Hussain Ather <shussainather@gmail.com>
2019-06-02 09:01:39 -0400

    count mismatches

--- constraint/qmc.py
+++ constraint/qmc.py
@@ -54,3 +54,18 @@ def decimal_to_binary(no_of_variable, minterms):
         temp.append(s)
 	s = ""
     return temp
+
+def is_for_table(string1, string2, count):
+    """
+    Are there count mismatches between the two strings?
+    """
+    l1 = list(string1);l2=list(string2)
+    count_n = 0 # Number of mismatches
+    for i in range(len(l1)):
+        if l1[i] != l2[i]:
+	    count_n += 1
+    if count_n == count:
+        return True
+    else:
+        return False 
+
