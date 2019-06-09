0495dd37f00a62b2ed4c7df660f4ea8f05da64b5
Syed Hussain Ather <shussainather@gmail.com>
2019-06-09 18:33:42 -0400

    init commit

---
+++ sort/externalmergesort.py
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct MinHeapNode
+{
+	// The element to be stored
+	int element;
+	// index of the array from which the element is taken
+	int i;
+};
+
