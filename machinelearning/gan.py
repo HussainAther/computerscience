0db6987ada46e134da2fcca8ed00979682a8af9c
Syed Hussain Ather <shussainather@gmail.com>
2019-06-15 01:28:41 -0400

    discriminator class

--- machinelearning/gan.py
+++ machinelearning/gan.py
@@ -37,6 +37,20 @@ class Generator(nn.Module):
         x = self.map3(x)
         return x
 
+class Discriminator(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, f):
+        super(Discriminator, self).__init__()
+        self.map1 = nn.Linear(input_size, hidden_size)
+        self.map2 = nn.Linear(hidden_size, hidden_size)
+        self.map3 = nn.Linear(hidden_size, output_size)
+        self.f = f
+
+    def forward(self, x):
+        x = self.f(self.map1(x))
+        x = self.f(self.map2(x))
+        return self.f(self.map3(x))
+
+
 # Data params
 data_mean = 4
 data_stddev = 1.25
