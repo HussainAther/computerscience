368e3a6a6bf31ed0b1779d8969ed6372c131759b
Syed Hussain Ather <shussainather@gmail.com>
2019-06-15 01:33:19 -0400

    iterate and train

--- machinelearning/gan.py
+++ machinelearning/gan.py
@@ -119,7 +119,36 @@ criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.htm
 d_optimizer = optim.SGD(D.parameters(), lr=d_learning_rate, momentum=sgd_momentum)
 g_optimizer = optim.SGD(G.parameters(), lr=g_learning_rate, momentum=sgd_momentum)
 
-
+for epoch in range(num_epochs):
+    for d_index in range(d_steps):
+        # Train D on real+fake
+        D.zero_grad()
+        # Train D on real
+        d_real_data = Variable(d_sampler(d_input_size))
+        d_real_decision = D(preprocess(d_real_data))
+        d_real_error = criterion(d_real_decision, Variable(torch.ones([1,1])))  # ones = true
+        d_real_error.backward() # compute/store gradients, but don't change params
+        #  Train D on fake
+        d_gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
+        d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
+        d_fake_decision = D(preprocess(d_fake_data.t()))
+        d_fake_error = criterion(d_fake_decision, Variable(torch.zeros([1,1])))  # zeros = fake
+        d_fake_error.backward()
+        d_optimizer.step() # Only optimizes D's parameters; changes based on stored gradients from backward()
+        dre, dfe = extract(d_real_error)[0], extract(d_fake_error)[0]
+    for g_index in range(g_steps):
+        # Train G on D's response (but DO NOT train D on these labels)
+        G.zero_grad()
+        gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
+        g_fake_data = G(gen_input)
+        dg_fake_decision = D(preprocess(g_fake_data.t()))
+        g_error = criterion(dg_fake_decision, Variable(torch.ones([1,1])))  # Train G to pretend it's genuine
+        g_error.backward()
+        g_optimizer.step()  # Only optimizes G's parameters
+        ge = extract(g_error)[0]
+    if epoch % print_interval == 0:
+        print("Epoch %s: D (%s real_err, %s fake_err) G (%s err); Real Dist (%s),  Fake Dist (%s) " %
+              (epoch, dre, dfe, ge, stats(extract(d_real_data)), stats(extract(d_fake_data))))
 
 print("Plotting the generated distribution...")
 values = extract(g_fake_data)
