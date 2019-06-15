8ce78ada3b3b311c99f3b6df2ddd8041990535ae
Syed Hussain Ather <shussainather@gmail.com>
2019-06-15 01:26:53 -0400

    import and init var

--- machinelearning/gan.py
+++ machinelearning/gan.py
@@ -1,6 +1,17 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.autograd import Variable
 
 """
+Generative adversarial neural network (GAN).
 """
+
+# Data params
+data_mean = 4
+data_stddev = 1.25
 
 print("Plotting the generated distribution...")
 values = extract(g_fake_data)
