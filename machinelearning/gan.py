b1813528730ff8f78b5b19f94de6bc0dd0b45b7b
Syed Hussain Ather <shussainather@gmail.com>
2019-06-15 01:31:04 -0400

    send to gen

--- machinelearning/gan.py
+++ machinelearning/gan.py
@@ -33,6 +33,20 @@ def stats(d):
     """
     return [np.mean(d), np.std(d)]
 
+def get_moments(d):
+    """
+    Return the first 4 moments of the data provided.
+    """
+    mean = torch.mean(d)
+    diffs = d - mean
+    var = torch.mean(torch.pow(diffs, 2.0))
+    std = torch.pow(var, 0.5)
+    zscores = diffs / std
+    skews = torch.mean(torch.pow(zscores, 3.0))
+    kurtoses = torch.mean(torch.pow(zscores, 4.0)) - 3.0  # excess kurtosis, should be 0 for Gaussian
+    final = torch.cat((mean.reshape(1,), std.reshape(1,), skews.reshape(1,), kurtoses.reshape(1,)))
+    return final
+
 class Generator(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, f):
         super(Generator, self).__init__()
@@ -62,10 +76,13 @@ class Discriminator(nn.Module):
         x = self.f(self.map2(x))
         return self.f(self.map3(x))
 
-
 # Data params
 data_mean = 4
 data_stddev = 1.25
+
+# send four moments to the generator.
+(name, preprocess, d_input_func) = ("Only 4 moments", lambda data: get_moments(data), lambda x: 4)
+
 
 print("Plotting the generated distribution...")
 values = extract(g_fake_data)
