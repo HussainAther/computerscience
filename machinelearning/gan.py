49db5a1a0edcf0887a6a50e143a883a678b270ea
Syed Hussain Ather <shussainather@gmail.com>
2019-06-15 01:28:17 -0400

    generator class

--- machinelearning/gan.py
+++ machinelearning/gan.py
@@ -21,6 +21,22 @@ def get_generator_input_sampler():
     """
     return lambda m, n: torch.rand(m, n)
 
+class Generator(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, f):
+        super(Generator, self).__init__()
+        self.map1 = nn.Linear(input_size, hidden_size)
+        self.map2 = nn.Linear(hidden_size, hidden_size)
+        self.map3 = nn.Linear(hidden_size, output_size)
+        self.f = f
+
+    def forward(self, x):
+        x = self.map1(x)
+        x = self.f(x)
+        x = self.map2(x)
+        x = self.f(x)
+        x = self.map3(x)
+        return x
+
 # Data params
 data_mean = 4
 data_stddev = 1.25
