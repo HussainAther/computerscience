f33af00909d794ad2585ce69f07ca2f7abfcb6ae
Syed Hussain Ather <shussainather@gmail.com>
2019-06-15 01:27:47 -0400

    input dissts

--- machinelearning/gan.py
+++ machinelearning/gan.py
@@ -9,6 +9,18 @@ from torch.autograd import Variable
 Generative adversarial neural network (GAN).
 """
 
+def get_distribution_sampler(mu, sigma):
+    """
+    Generate input distribution Gaussian.
+    """
+    return lambda n: torch.Tensor(np.random.normal(mu, sigma, (1, n)))
+
+def get_generator_input_sampler():
+    """
+    Input sampler for choosing from the distribution.
+    """
+    return lambda m, n: torch.rand(m, n)
+
 # Data params
 data_mean = 4
 data_stddev = 1.25
