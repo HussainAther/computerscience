ce0f6bac26358abd43956a4b24cebaff9d563f09
Syed Hussain Ather <shussainather@gmail.com>
2019-06-17 10:34:36 -0400

    comment

--- graph/isorank.py
+++ graph/isorank.py
@@ -6,5 +6,6 @@ method for non-attributed networks.
 def isorank(a1, a2, h, alpha, maxiter, tol):
     """
     For two adjacency matrices a1 and a2 with a prior node similarity matrix h, alpha
-    decay factor, and maximum number of iterations maxiter.
+    decay factor, and maximum number of iterations maxiter. Output S, an alignment matrix
+    that represents to what extent node x in a2 is aligned to node y in a1.
     """
