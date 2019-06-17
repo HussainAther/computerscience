e353905d224651a5a58040b9671d3ab2bbc558ef
Syed Hussain Ather <shussainather@gmail.com>
2019-06-17 10:37:55 -0400

    normalize

--- graph/isorank.py
+++ graph/isorank.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 IsoRank algorithm computes the alignment (similarity) matrix by a random walk-based
 method for non-attributed networks.
@@ -9,5 +11,7 @@ def isorank(a1, a2, h, alpha, maxiter, tol):
     decay factor, and maximum number of iterations maxiter. Output S, an alignment matrix
     that represents to what extent node x in a2 is aligned to node y in a1.
     """
-    n1 = a1.size
+    n1 = a1.size # Get the matrix sizes.
     n2 = a2.size
+    d1 = 1/sum(a1, 2) # Sum the matrix with 2 to normalize.
+    d2 = 1/sum(a2)
