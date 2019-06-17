5f567d4735df6b42a7b3a3403c690f20b758e21c
Syed Hussain Ather <shussainather@gmail.com>
2019-06-17 10:53:54 -0400

    def tens

--- graph/3modetensor.py
+++ graph/3modetensor.py
@@ -4,3 +4,13 @@ import numpy as np
 3-mode tensor with a map that picks the largest magnitude real eigenvalue and numerical 
 integration with the forward Euler method.
 """
+
+def tensorapply(T, x):
+    """
+    For some array tensor T and vector field x, apply the tensor.
+    """
+    n = len(x)
+    y = np.zeros(n)
+    for k in range(n):
+        y += T[::k] * x * x[k]
+    return y
