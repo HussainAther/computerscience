a6cbc304f4ff696a37750c41fe84d6f03707e87f
Syed Hussain Ather <shussainather@gmail.com>
2019-06-17 10:33:55 -0400

    Def

--- graph/isorank.py
+++ graph/isorank.py
@@ -2,3 +2,9 @@
 IsoRank algorithm computes the alignment (similarity) matrix by a random walk-based
 method for non-attributed networks.
 """
+
+def isorank(a1, a2, h, alpha, maxiter, tol):
+    """
+    For two adjacency matrices a1 and a2 with a prior node similarity matrix h, alpha
+    decay factor, and maximum number of iterations maxiter.
+    """
