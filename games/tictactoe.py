0a3250a807b53c775f343927c270c6f5eaafb6ad
Syed Hussain Ather <shussainather@gmail.com>
2019-06-30 13:04:50 -0400

    hash

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -10,13 +10,24 @@ BOARD_COLS = 3
 BOARD_SIZE = BOARD_ROWS * BOARD_COLS
 
 class State:
+    """
+    The board is represented by an n * n array,
+    1 represents a chessman of the player who moves first,
+    -1 represents a chessman of another player.
+    0 represents an empty position.
+    """
     def __init__(self):
-        # The board is represented by an n * n array,
-        # 1 represents a chessman of the player who moves first,
-        # -1 represents a chessman of another player
-        # 0 represents an empty position
         self.data = np.zeros((BOARD_ROWS, BOARD_COLS))
         self.winner = None
         self.hash_val = None
         self.end = None:w
 
+    def hash(self):
+        """
+        Compute the hash value for one state.
+        """
+        if self.hash_val is None:
+            self.hash_val = 0
+            for i in np.nditer(self.data):
+                self.hash_val = self.hash_val * 3 + i + 1
+        return self.hash_val
