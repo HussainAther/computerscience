f4e58d00309c510ce77976866b0a8928c749ee1a
Syed Hussain Ather <shussainather@gmail.com>
2019-06-30 13:09:47 -0400

    play

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -111,8 +111,10 @@ def get_all_states():
 all_states = get_all_states()
 
 class Judger:
-    # @player1: the player who will move first, its chessman will be 1
-    # @player2: another player with a chessman -1
+    """
+    player1: the player who will move first, its chessman will be 1.
+    player2: another player with a chessman -1.
+    """
     def __init__(self, player1, player2):
         self.p1 = player1
         self.p2 = player2
@@ -132,3 +134,22 @@ class Judger:
             yield self.p1
             yield self.p2
 
+    def play(self, print_state=False):
+        alternator = self.alternate()
+        self.reset()
+        current_state = State()
+        self.p1.set_state(current_state)
+        self.p2.set_state(current_state)
+        if print_state:
+            current_state.print_state()
+        while True:
+            player = next(alternator)
+            i, j, symbol = player.act()
+            next_state_hash = current_state.next_state(i, j, symbol).hash()
+            current_state, is_end = all_states[next_state_hash]
+            self.p1.set_state(current_state)
+            self.p2.set_state(current_state)
+            if print_state:
+                current_state.print_state()
+            if is_end:
+                return current_state.winner
