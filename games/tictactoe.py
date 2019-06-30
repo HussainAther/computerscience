11efb194e0347ac2c8e64dffded7b17b0d549e05
Syed Hussain Ather <shussainather@gmail.com>
2019-06-30 13:03:56 -0400

    class

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -8,3 +8,15 @@ Tic-Tac-Toe implementation using reinforcement learning.
 BOARD_ROWS = 3
 BOARD_COLS = 3
 BOARD_SIZE = BOARD_ROWS * BOARD_COLS
+
+class State:
+    def __init__(self):
+        # The board is represented by an n * n array,
+        # 1 represents a chessman of the player who moves first,
+        # -1 represents a chessman of another player
+        # 0 represents an empty position
+        self.data = np.zeros((BOARD_ROWS, BOARD_COLS))
+        self.winner = None
+        self.hash_val = None
+        self.end = None:w
+
