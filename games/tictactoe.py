91a128e6a356d7fb300a47233c4f7d237b296c2e
Syed Hussain Ather <shussainather@gmail.com>
2019-06-30 13:11:20 -0400

    class player

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -153,3 +153,86 @@ class Judger:
                 current_state.print_state()
             if is_end:
                 return current_state.winner
+
+class Player:
+    """
+    step_size: the step size to update estimations.
+    epsilon: the probability to explore.
+    """
+    def __init__(self, step_size=0.1, epsilon=0.1):
+        self.estimations = dict()
+        self.step_size = step_size
+        self.epsilon = epsilon
+        self.states = []
+        self.greedy = []
+        self.symbol = 0
+
+    def reset(self):
+        self.states = []
+        self.greedy = []
+
+    def set_state(self, state):
+        self.states.append(state)
+        self.greedy.append(True)
+
+    def set_symbol(self, symbol):
+        self.symbol = symbol
+        for hash_val in all_states:
+            state, is_end = all_states[hash_val]
+            if is_end:
+                if state.winner == self.symbol:
+                    self.estimations[hash_val] = 1.0
+                elif state.winner == 0:
+                    # we need to distinguish between a tie and a lose
+                    self.estimations[hash_val] = 0.5
+                else:
+                    self.estimations[hash_val] = 0
+            else:
+                self.estimations[hash_val] = 0.5
+
+    # update value estimation
+    def backup(self):
+        states = [state.hash() for state in self.states]
+
+        for i in reversed(range(len(states) - 1)):
+            state = states[i]
+            td_error = self.greedy[i] * (
+                self.estimations[states[i + 1]] - self.estimations[state]
+            )
+            self.estimations[state] += self.step_size * td_error
+
+    # choose an action based on the state
+    def act(self):
+        state = self.states[-1]
+        next_states = []
+        next_positions = []
+        for i in range(BOARD_ROWS):
+            for j in range(BOARD_COLS):
+                if state.data[i, j] == 0:
+                    next_positions.append([i, j])
+                    next_states.append(state.next_state(
+                        i, j, self.symbol).hash())
+
+        if np.random.rand() < self.epsilon:
+            action = next_positions[np.random.randint(len(next_positions))]
+            action.append(self.symbol)
+            self.greedy[-1] = False
+            return action
+
+        values = []
+        for hash_val, pos in zip(next_states, next_positions):
+            values.append((self.estimations[hash_val], pos))
+        # to select one of the actions of equal value at random due to Python's sort is stable
+        np.random.shuffle(values)
+        values.sort(key=lambda x: x[0], reverse=True)
+        action = values[0][1]
+        action.append(self.symbol)
+        return action
+
+    def save_policy(self):
+        with open("policy_%s.bin" % ("first" if self.symbol == 1 else "second"), "wb") as f:
+            pickle.dump(self.estimations, f)
+
+    def load_policy(self):
+        with open("policy_%s.bin" % ("first" if self.symbol == 1 else "second"), "rb") as f:
+            self.estimations = pickle.load(f)
