8b6b6411d6f9c812035c04d0284e22beabc3ae75
Syed Hussain Ather <shussainather@gmail.com>
2019-07-16 01:59:21 -0400

    rm paren

--- misc/regexp.py
+++ misc/regexp.py
@@ -6,3 +6,6 @@ Regexp practice.
 
 # Insert spaces between words with capital letters.
 re.sub(r"(\w)([A-Z])", r"\1 \2", str1)
+
+# Remove parenthesis area in a string.
+re.sub(r" ?\([^)]+\)", "", str1)
