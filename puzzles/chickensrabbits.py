b9336283c47427f558cf75cf64be86e8242e6324
Syed Hussain Ather <shussainather@gmail.com>
2019-07-16 01:55:43 -0400

    init var

--- puzzles/chickensrabbits.py
+++ puzzles/chickensrabbits.py
@@ -3,3 +3,7 @@ Write a program to solve a classic ancient Chinese puzzle:
 We count 35 heads and 94 legs among the chickens and rabbits in a farm. 
 How many rabbits and how many chickens do we have?
 """
+
+h = 35
+l = 94
+
