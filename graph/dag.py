79b5240fb0bdb7c230c09ebf8bbcd8d76ec5d4ad
Syed Hussain Ather <shussainather@gmail.com>
2019-07-29 05:32:37 -0400

    edge labels

--- graph/dag.py
+++ graph/dag.py
@@ -21,3 +21,8 @@ G.add_edges_from([("T", "K"), ("T", "H"), ("T", "H")], weight=4)
 # These values determine node colors.
 val_map = {"L": 1.5, "I": .9, "R": .6, "T":.2}
 values = [val_map.get(node, 1.0) for node in G.nodes()]
+
+# Label the edges.
+edges_labels = dict([((u, v), d["weight"]) for u, v, d in G.edges(data=True)])
+
+
