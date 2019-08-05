a878bdc882ff7966a672a6bcd5e1e21bc21d773b
Syed Hussain Ather <shussainather@gmail.com>
2019-08-05 10:30:34 -0400

    return p

--- graph/binary.py
+++ graph/binary.py
@@ -11,9 +11,23 @@ class TreeMap(LinkedBinaryTree, MapBase):
             """
             Return key of map's key-value pair.
             """
-            return self.element()._key
+            return self.element().key
         def value(self):
             """
             Return value of map's key-value pair.
             """
-            return self.element()._value
+            return self.element().value
+
+def subtreesearch(self, p, k):
+    """
+    Return Posiiton of p's subtree having key k or last node searched.
+    """
+    if k == p.key():
+        return p
+    elif k < p.key():
+        if self.left(p) is not None:
+            return self.subtreesearch(self.left(p), k)
+    else:
+        if self.right(p) is not None:
+            return self.subtreesearch(self.right(p), k)
+    return p
