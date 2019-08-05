ba13195c55ada91ab640494c9686d0a25767c725
Syed Hussain Ather <shussainather@gmail.com>
2019-08-05 10:34:20 -0400

    walk

--- graph/binary.py
+++ graph/binary.py
@@ -18,16 +18,34 @@ class TreeMap(LinkedBinaryTree, MapBase):
             """
             return self.element().value
 
-def subtreesearch(self, p, k):
-    """
-    Return Posiiton of p's subtree having key k or last node searched.
-    """
-    if k == p.key():
+    def subtreesearch(self, p, k):
+        """
+        Return Posiiton of p's subtree having key k or last node searched.
+        """
+        if k == p.key():
+            return p
+        elif k < p.key():
+            if self.left(p) is not None:
+                return self.subtreesearch(self.left(p), k)
+        else:
+            if self.right(p) is not None:
+                return self.subtreesearch(self.right(p), k)
         return p
-    elif k < p.key():
-        if self.left(p) is not None:
-            return self.subtreesearch(self.left(p), k)
-    else:
-        if self.right(p) is not None:
-            return self.subtreesearch(self.right(p), k)
-    return p
+
+    def subtreefirstposition(self, p):
+        """
+        Return position of first item in subtree rooted at p.
+        """
+        walk = p 
+        while self.left(walk) is not None:
+            walk = self.left(walk)
+        return walk
+
+    def subtreelastposition(self, p):
+        """
+        Return position of last item in subtree rooted at p.
+        """
+        walk = p
+        while self.right(walk) is not None:
+            walk = self.right(walk)
+        return walk
