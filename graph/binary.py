70b3f04a76cc71faf7711cd17e6978625f2efd1a
Syed Hussain Ather <shussainather@gmail.com>
2019-08-05 10:26:50 -0400

    return key

--- graph/binary.py
+++ graph/binary.py
@@ -6,3 +6,9 @@ class TreeMap(LinkedBinaryTree, MapBase):
     """
     Sorted map implementation using a binary search tree. 
     """
+    class Position(LinkedBinaryTree.Position):
+        def key(self):
+            """
+            Return key of map's key-value pair.
+            """
+            return self.element()._key
