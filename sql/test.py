a3010cf985c34896a9f375981cc1e772891e1244
Syed Hussain Ather <shussainather@gmail.com>
2019-08-27 14:51:19 -0400

    statement

--- sql/test.py
+++ sql/test.py
@@ -6,3 +6,8 @@ MySQL stuff.
 """
 
 mydb = MySQLdb.connect(host="localhost", user="logic", passwd="nottelling", db="fish")
+cur = mydb.cursor()
+
+identifier = sys.argv[1]
+
+statement = """SELECT * FROM menu WHERE id=%s"""%(identifier)
