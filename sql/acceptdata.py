2620008582a068218205d7507bd74f5f1d01d571
Syed Hussain Ather <shussainather@gmail.com>
2019-08-27 15:54:52 -0400

    option

--- sql/acceptdata.py
+++ sql/acceptdata.py
@@ -8,3 +8,5 @@ opt = optparse.OptionParser()
 opt.add_option("-a", "--actor", action="store", help="denotes the last-na/esur-name of the " \
                "last-name/sur-name of the actor for searc - only ONE of actor or film can " \
                "be used at a time", dest="actor")
+opt.add_option("-f", "--film", action="store", help="denotes film for search", dest="film")
+opt, args = opt.parse_args()
