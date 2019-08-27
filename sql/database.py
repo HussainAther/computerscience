830d20922ca9d1507236f94b286adac376980f31
Syed Hussain Ather <shussainather@gmail.com>
2019-08-27 18:21:56 -0400

    start the class

--- sql/database.py
+++ sql/database.py
@@ -1,3 +1,12 @@
+
+
 """
 Database class.
 """
+
+class Database:
+    def __init__(self):
+         """
+         A class representation for MySQL database metadata.
+         """
+         self.database = []
