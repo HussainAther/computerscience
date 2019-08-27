2f5b4d1d76db8169254882029e728b437124d6e8
Syed Hussain Ather <shussainather@gmail.com>
2019-08-27 18:26:32 -0400

    return

--- sql/database.py
+++ sql/database.py
@@ -6,7 +6,24 @@ Database class.
 
 class Database:
     def __init__(self):
-         """
-         A class representation for MySQL database metadata.
-         """
-         self.database = []
+        """
+        A class representation for MySQL database metadata.
+        """
+        self.database = []
+        
+    def tables(self, cursor):
+        """
+        Returns a list of the database tables.
+        """
+        statement = "SHOW TABLES"
+        header = ("Tables")
+    
+    try:
+        runit = cursor.execute(statement)
+        results = cursor.fetchall()
+    except MySQLdb.Error as e:
+        results = "The query you attempted failed. " \
+                  "Please verify the information you " \
+                  "have submitted and try again. The " \
+                  "error message that was received reads: %s " % (e)
+        return header, results 
