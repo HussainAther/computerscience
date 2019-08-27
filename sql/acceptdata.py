84ede85e5fb121a22ee61d0571c786ce614460d4
Syed Hussain Ather <shussainather@gmail.com>
2019-08-27 15:53:32 -0400

    actor

--- sql/acceptdata.py
+++ sql/acceptdata.py
@@ -5,3 +5,6 @@ Accept user data.
 """
 
 opt = optparse.OptionParser()
+opt.add_option("-a", "--actor", action="store", help="denotes the last-na/esur-name of the " \
+               "last-name/sur-name of the actor for searc - only ONE of actor or film can " \
+               "be used at a time", dest="actor")
