7f478da59ce8533831cb7417ccadc02f5e0f7c66
Syed Hussain Ather <shussainather@gmail.com>
2019-11-04 10:03:19 -0800

    class

--- machinelearning/ganv2.py
+++ machinelearning/ganv2.py
@@ -13,3 +13,11 @@ from keras.optimizers import Adam
 """
 Generative adversarial network (GAN).
 """
+
+class GAN():
+    def __init__(self):
+        self.img_rows = 28
+        self.img_cols = 28
+        self.channels = 1
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        self.latent_dim = 100
