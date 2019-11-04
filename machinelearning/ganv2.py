4572a5b2f486a039c15c63b2373ca15c12defc4f
Syed Hussain Ather <shussainather@gmail.com>
2019-11-04 10:10:46 -0800

    train the dis

--- machinelearning/ganv2.py
+++ machinelearning/ganv2.py
@@ -80,4 +80,17 @@ class GAN():
         # Adversarial ground truths
         valid = np.ones((batch_size, 1))
         fake = np.zeros((batch_size, 1))
-
+        for epoch in range(epochs):
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+            # Select a random batch of images
+            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            imgs = X_train[idx]
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+            # Generate a batch of new images
+            gen_imgs = self.generator.predict(noise)
+            # Train the discriminator
+            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
