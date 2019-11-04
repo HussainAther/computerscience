d00b27f815a67f72aeba6a275c2d6973dfd76062
Syed Hussain Ather <shussainather@gmail.com>
2019-11-04 10:06:13 -0800

    build gen

--- machinelearning/ganv2.py
+++ machinelearning/ganv2.py
@@ -40,3 +40,17 @@ class GAN():
         # Trains the generator to fool the discriminator
         self.combined = Model(z, validity)
         self.combined.compile(loss="binary_crossentropy", optimizer=optimizer)
+    def build_generator(self):
+        model = Sequential()
+        model.add(Dense(256, input_dim=self.latent_dim))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(1024))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
+        model.add(Reshape(self.img_shape))
+
