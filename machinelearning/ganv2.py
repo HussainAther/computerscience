eca40fc98586a4711b0a4aca180bc6bdbcad7223
Syed Hussain Ather <shussainather@gmail.com>
2019-11-04 10:11:15 -0800

    save

--- machinelearning/ganv2.py
+++ machinelearning/ganv2.py
@@ -94,3 +94,14 @@ class GAN():
             d_loss_real = self.discriminator.train_on_batch(imgs, valid)
             d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+            # Train the generator (to have the discriminator label samples as valid)
+            g_loss = self.combined.train_on_batch(noise, valid)
+            # Plot the progress
+            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                self.sample_images(epoch)
