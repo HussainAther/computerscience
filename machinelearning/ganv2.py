2b7757fdf9e8d9907bd91681521ff424fffeb7fa
Syed Hussain Ather <shussainather@gmail.com>
2019-11-04 10:07:33 -0800

    build dis

--- machinelearning/ganv2.py
+++ machinelearning/ganv2.py
@@ -57,3 +57,16 @@ class GAN():
         noise = Input(shape=(self.latent_dim,))
         img = model(noise)
         return Model(noise, img)
+
+   def build_discriminator(self):
+        model = Sequential()
+        model.add(Flatten(input_shape=self.img_shape))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(256))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(1, activation='sigmoid'))
+        model.summary()
+        img = Input(shape=self.img_shape)
+        validity = model(img)
+        return Model(img, validity)
