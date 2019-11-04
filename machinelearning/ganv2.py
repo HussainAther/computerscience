241583abd989f18c48b2c7a4971b2bbb93f8f45d
Syed Hussain Ather <shussainather@gmail.com>
2019-11-04 09:58:00 -0800

    import

--- machinelearning/ganv2.py
+++ machinelearning/ganv2.py
@@ -1,5 +1,10 @@
-
-
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
 
 """
 Generative adversarial network (GAN).
