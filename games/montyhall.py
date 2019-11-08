938e51e4d83d680633019958432a96cfda4b596b
Syed Hussain Ather <shussainather@gmail.com>
2019-11-07 18:10:13 -0800

    pmf

--- games/montyhall.py
+++ games/montyhall.py
@@ -1,3 +1,14 @@
+from thinkbayes import Pmf
+
 """
 Bayesian framework for Monty Hall problem.
+Use probability mass function (Pmf).
 """
+
+class Monty(Pmf):
+    def __init__(self, hypos):
+    Pmf.__init__(self)
+    for hypo in hypos:
+        self.Set(hypo, 1)
+         self.Normalize()
+
