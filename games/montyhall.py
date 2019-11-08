8ec68890df9d5bc04c348a70c15298043c4cb138
Syed Hussain Ather <shussainather@gmail.com>
2019-11-07 19:00:59 -0800

    update

--- games/montyhall.py
+++ games/montyhall.py
@@ -10,10 +10,16 @@ class Monty(Pmf):
     Test various hypotheses in the problem. 
     """
     def __init__(self, hypos):
-    Pmf.__init__(self)
-    for hypo in hypos:
-        self.Set(hypo, 1)
-    self.Normalize()
-    hypos = "ABC"
-    pmf = Monty(hypos)
-
+        Pmf.__init__(self)
+        for hypo in hypos:
+            self.Set(hypo, 1)
+        self.Normalize()
+        hypos = "ABC"
+        pmf = Monty(hypos)
+        data = "B"
+        pmf.Update(data)
+    
+    def Update(self, data):
+        """
+        Update hte hypotheses given data.
+        """ 
