3a25b260812e654252d0386fc168bf45f8e228fe
Syed Hussain Ather <shussainather@gmail.com>
2019-11-21 23:04:51 -0800

    return comment

--- machinelearning/wao.py
+++ machinelearning/wao.py
@@ -1,7 +1,16 @@
 import numpy as np
-
-from random import random
+import random
 
 """
 Whale optimization algorithm (WAO) based on how whales hunt for food.
 """
+
+def wao(sano, maxiter, lb, ub, dim, fobj)
+    """
+    The optimization algorithm with sano as number of search agents,
+    lower boundary lb, upper boundary ub (for the boundary of the search space),
+    dim dimensions and fobj objective function.
+   
+    Return the lscore leader score, lpos leader position, and conv convergence curve.
+    """
+
