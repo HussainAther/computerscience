87ba48b914523144e5d3815485362f6b9358592d
Syed Hussain Ather <shussainather@gmail.com>
2019-11-22 23:17:07 -0800

    start it up

--- machinelearning/artificialnn.py
+++ machinelearning/artificialnn.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+from scipy.stats import truncnorm
+
 """
 Artificial neural network (ANN) implementation.
 """
+
+@np.vectorize
+def sigmoid(x):
+    return 1 / (1 + np.e ** -x)
+activation_function = sigmoid
