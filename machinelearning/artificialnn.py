ca6c02874a0d9be0e19b2c578d96e5a515bf39a0
Syed Hussain Ather <shussainather@gmail.com>
2019-11-22 23:19:16 -0800

    class

--- machinelearning/artificialnn.py
+++ machinelearning/artificialnn.py
@@ -20,3 +20,16 @@ def truncated_normal(mean=0, sd=1, low=0, upp=10):
     """
     return truncnorm(
         (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
+
+class NeuralNetwork:
+    def __init__(self, 
+                 no_of_in_nodes, 
+                 no_of_out_nodes, 
+                 no_of_hidden_nodes,
+                 learning_rate):
+        self.no_of_in_nodes = no_of_in_nodes
+        self.no_of_out_nodes = no_of_out_nodes
+        self.no_of_hidden_nodes = no_of_hidden_nodes
+        self.learning_rate = learning_rate 
+        self.create_weight_matrices()
+        
