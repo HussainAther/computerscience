2024c9e4e6c955ca82aa53a79c15dea61cc06a83
Syed Hussain Ather <shussainather@gmail.com>
2019-11-22 23:18:08 -0800

    Truncate

--- machinelearning/artificialnn.py
+++ machinelearning/artificialnn.py
@@ -8,5 +8,15 @@ Artificial neural network (ANN) implementation.
 
 @np.vectorize
 def sigmoid(x):
+    """
+    Sigmoid function for activation in optimization problems
+    """
     return 1 / (1 + np.e ** -x)
 activation_function = sigmoid
+
+def truncated_normal(mean=0, sd=1, low=0, upp=10):
+    """
+    Truncate it with mean, standard deviation, lower and upper bound.
+    """
+    return truncnorm(
+        (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
