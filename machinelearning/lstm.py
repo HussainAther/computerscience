cec3876c7766f6b118eec4d014c7264d330d063c
Syed Hussain Ather <shussainather@gmail.com>
2019-11-29 16:50:49 -0800

    it's like paragrphas

--- machinelearning/lstm.py
+++ machinelearning/lstm.py
@@ -39,4 +39,49 @@ dataset = scaler.fit_transform(dataset)
 train_size = int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-print(len(train), len(test))
+
+# Reshape into X=t and Y=t+1.
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# Reshape input to be [samples, time steps, features].
+trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+# Create and fit the LSTM network.
+model = Sequential()
+model.add(LSTM(4, input_shape=(1, look_back)))
+model.add(Dense(1))
+model.compile(loss="mean_squared_error", optimizer="adam")
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
+# Predict.
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+# Invert predictions.
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+# Calculate root mean squared error.
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+
+# Shift train predictions for plotting.
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+# Shift test predictions for plotting.
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
+# Plot.
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
