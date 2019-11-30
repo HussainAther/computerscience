66edb8601b691e4dc544cfe8decb2192d4ab7cf6
Syed Hussain Ather <shussainather@gmail.com>
2019-11-29 16:45:30 -0800

    init

---
+++ machinelearning/lstm.py
@@ -0,0 +1,14 @@
+import numpy
+import matplotlib.pyplot as plt
+import pandas
+import math
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+"""
+Long Short-Term (long short term) Memory network or LSTM.
+"""
