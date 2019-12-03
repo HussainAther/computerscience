305fb598296cb993947ffc660bcfbcc004d4796d
Syed Hussain Ather <shussainather@gmail.com>
2019-12-02 19:43:26 -0800

    alphabetize imports

--- graph/voronoi.py
+++ graph/voronoi.py
@@ -1,9 +1,9 @@
-import numpy as np
-import pandas as pd
-import scipy.spatial as spatial
 import matplotlib.pyplot as plt
 import matplotlib.path as path
 import matplotlib as mpl
+import numpy as np
+import pandas as pd
+import scipy.spatial as spatial
 
 """
 Voronoi diagrams let us divide spcae into several regions such that each region
