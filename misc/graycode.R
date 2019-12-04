97fd5a050d9e2213aa2d42cc1b1d49987e0acf7a
Syed Hussain Ather <shussainather@gmail.com>
2019-12-04 10:34:20 -0800

    gry

--- misc/graycode.R
+++ misc/graycode.R
@@ -1,11 +1,9 @@
-"""
-Gray code is a form of binary encoding where transitions 
+"Gray code is a form of binary encoding where transitions 
 between consecutive numbers differ by only one bit. This is a 
 useful encoding for reducing hardware data hazards with values 
 that change rapidly and/or connect to slower hardware as inputs. 
 It is also useful for generating inputs for Karnaugh maps in 
-order from left to right or top to bottom.
-"""
+order from left to right or top to bottom."
 
 GrayEncode <- function(binary) {
 	gray <- substr(binary,1,1)
