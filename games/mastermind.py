c8ef0a0f578e20c233b926c86ae2eeffee4a4c78
Syed Hussain Ather <shussainather@gmail.com>
2019-12-12 02:41:20 -0800

    def encode

--- games/mastermind.py
+++ games/mastermind.py
@@ -5,3 +5,8 @@ Python implementation of Mastermind, a puzzle game
 in which the user must guess a 4-character pattern
 based on feedback from a computer.
 """
+
+def encode(c, g):
+    """
+    Return the correct c based on guess g of the user.
+    """
