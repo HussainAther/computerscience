303b1c98855d10c1137a7de78d2a9d06e7369791
Syed Hussain Ather <shussainather@gmail.com>
2020-01-14 02:04:49 -0800

    init layer

--- machinelearning/xavier.py
+++ machinelearning/xavier.py
@@ -6,3 +6,9 @@ Xavier initialization
 """
 
 w = np.random.randn(layer_size[l],layer_size[l-1])*np.sqrt(1/layer_size[l-1])
+
+tf.contrib.layers.xavier_initializer(
+    uniform=True,
+    seed=None,
+    dtype=tf.float32
+)
