d9cba85b1d21abe57f2452a760757ff9ec57a2a8
Syed Hussain Ather <shussainather@gmail.com>
2020-02-29 12:52:59 -0500

    finish later

--- graph/factorproduct.py
+++ graph/factorproduct.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Factor-product (factor product)
 Use a factor (the basic data structure) or a multi-dimensional table
@@ -6,9 +8,19 @@ tables, we can flatten them into a single array in computer memory. For each var
 we can store its cardinality and its stride (step size in the factor).
 """
 
-def fp(phi1, phi2):
+def fp(phi1, X1, phi2, X2):
     """
     For phi1 over scope X1 and phi2 over scope X2 (factors represented
     as a flat array with strides for the variables), perform the 
     factor-product operation.   
     """
+    j = 0
+    k = 0
+    l = []
+    psi = []
+    for i in range(X1.union(X2)[-1]):
+        l[i] = 0 
+    for i in range(X1.union(X2)[-1]-1):
+        psi[i] = phi1[j]*phi2[k]
+        for l in range(X1.union(X2)[-1]):
+            l = l + 1
