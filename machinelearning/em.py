96134ac41fba92860f25bc2b1f0be91e38d139b8
Syed Hussain Ather <shussainather@gmail.com>
2020-03-10 15:24:07 -0400

    def it

--- machinelearning/em.py
+++ machinelearning/em.py
@@ -16,3 +16,18 @@ expectation-maximization (EM) from University of Washington Coursera's
 
 # Load dataset.
 wiki = pd.read_csv("people_wiki.csv")
+
+def load_sparse_csr(filename):
+    """
+    Load the dataset as instructed.
+    """
+    loader = np.load(filename)
+    data = loader["data"]
+    indices = loader["indices"]
+    indptr = loader["indptr"]
+    shape = loader["shape"]
+    
+    return csr_matrix( (data, indices, indptr), shape)
+
+tf_idf = load_sparse_csr("4_tf_idf.npz")  # NOT people_wiki_tf_idf.npz
+map_index_to_word = pd.read_json("4_map_index_to_word.json",typ="series")  # NOT people_wiki_map_index_to_word.gl
