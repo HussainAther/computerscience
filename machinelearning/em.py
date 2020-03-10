f2713520b2b2d6ce94774ec292d1ca669074fbfe
Syed Hussain Ather <shussainather@gmail.com>
2020-03-10 15:22:09 -0400

    import

--- machinelearning/em.py
+++ machinelearning/em.py
@@ -1,3 +1,13 @@
+import numpy as np
+import pandas as pd
+
+from copy import deepcopy
+from scipy.sparse import csr_matrix
+from scipy.sparse import spdiags
+from scipy.stats import multivariate_normal
+from sklearn.metrics import pairwise_distances
+from sklearn.preprocessing import normalize
+
 """
 Fitting a diagonal covariance Gaussian mixture model to text data using 
 expectation-maximization (EM) from University of Washington Coursera's
