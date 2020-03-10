8c52e3d5cdb65cb12a938fc6e0071f0d12e3c0ce
Syed Hussain Ather <shussainather@gmail.com>
2020-03-10 15:07:49 -0400

    tfidf

--- machinelearning/hierarch.py
+++ machinelearning/hierarch.py
@@ -14,3 +14,17 @@ Hierarchical clustering from University of Washington Coursera's
 
 # Load the dataset.
 wiki = pd.read_csv("people_wiki.csv")
+
+def load_sparse_csr(filename):
+    """
+    Extract TF-IDF features (term frequency–inverse document frequency).
+    """ 
+    loader = np.load(filename)
+    data = loader["data"]
+    indices = loader["indices"]
+    indptr = loader["indptr"]
+    shape = loader["shape"]
+    return csr_matrix( (data, indices, indptr), shape)
+
+tf_idf = load_sparse_csr("people_wiki_tf_idf.npz")
+map_index_to_word = pd.read_json("people_wiki_map_index_to_word.json", typ="series")
