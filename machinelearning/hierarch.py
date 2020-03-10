7d280772b8294392df77c2c53627ee5bc317b5c3
Syed Hussain Ather <shussainather@gmail.com>
2020-03-10 15:12:13 -0400

    sk kmeans

--- machinelearning/hierarch.py
+++ machinelearning/hierarch.py
@@ -40,3 +40,17 @@ def bipartition(cluster, maxiter=400, num_runs=4, seed=None):
     """
     data_matrix = cluster["matrix"]
     dataframe   = cluster["dataframe"]
+
+    # Run k-means on the data matrix with k=2. We use scikit-learn here to simplify workflow.
+    kmeans_model = KMeans(n_clusters=2, max_iter=maxiter, n_init=num_runs, random_state=seed, n_jobs=-1,verbose=1)
+    kmeans_model.fit(data_matrix)
+    centroids, cluster_assignment = kmeans_model.cluster_centers_, kmeans_model.labels_
+    
+    # Divide the data matrix into two parts using the cluster assignments.
+    data_matrix_left_child, data_matrix_right_child = data_matrix[cluster_assignment==0], \
+                                                      data_matrix[cluster_assignment==1]
+    
+    # Divide the dataframe into two parts, again using the cluster assignments.
+    cluster_assignment_sa = np.array(cluster_assignment) # minor format conversion
+    dataframe_left_child, dataframe_right_child     = dataframe[cluster_assignment_sa==0], \
+                                                      dataframe[cluster_assignment_sa==1]
