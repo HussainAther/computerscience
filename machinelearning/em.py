8cade2cc31d9212479ecd95aa372a5ab8aa74dbf
Syed Hussain Ather <shussainather@gmail.com>
2020-03-10 15:21:35 -0400

    init

---
+++ machinelearning/em.py
@@ -0,0 +1,5 @@
+"""
+Fitting a diagonal covariance Gaussian mixture model to text data using 
+expectation-maximization (EM) from University of Washington Coursera's
+"Machine Learning: Clustering & Retrieval"
+"""
