84128c966edee2f36f560a7be9d5b7d53953118d
Syed Hussain Ather <shussainather@gmail.com>
2020-04-02 15:52:58 -0400

    test it

--- machinelearning/gdt.py
+++ machinelearning/gdt.py
@@ -10,3 +10,9 @@ def sum_python(N):
     Vector sum
     """
     return np.sum(np.arange(N)**2)
+
+# An integer parameter
+N = tf.placeholder("int64", name="input_to_your_function")
+
+# A recipe on how to produce the same result
+result = tf.reduce_sum(tf.range(N)**2)
