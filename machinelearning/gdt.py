e05f9c6c6ae0ba950988979ba2fa0e55427cb19e
Syed Hussain Ather <shussainather@gmail.com>
2020-04-02 15:52:15 -0400

    vector sum

--- machinelearning/gdt.py
+++ machinelearning/gdt.py
@@ -1,3 +1,12 @@
+import numpy as np
+import tensorflow as tf
+
 """
 Gradient descent trajectory (gdt) in tensorflow 
 """
+
+def sum_python(N):
+    """
+    Vector sum
+    """
+    return np.sum(np.arange(N)**2)
