4bee29c5e60fe97d082ef2510f1953bdcda81c96
Syed Hussain Ather <shussainather@gmail.com>
2020-04-02 18:39:19 -0400

    import

--- machinelearning/cnn.py
+++ machinelearning/cnn.py
@@ -1,3 +1,11 @@
+import keras
+import keras_utils
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from keras import backend as K
+
 """
 Convolutional neural network (CNN) in CIFAR-10 dataset
 """
