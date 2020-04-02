8f61fefe77b1ca3e582e2e6854bf582ed121a88d
Syed Hussain Ather <shussainather@gmail.com>
2020-04-02 18:42:06 -0400

    fit

--- machinelearning/cnn.py
+++ machinelearning/cnn.py
@@ -37,29 +37,29 @@ def make_model():
 
     # CONV 1
     # first layer needs to define "input_shape"
-    model.add(Conv2D(16, (3, 3), strides = (1, 1), padding="same", name = 'conv1', input_shape=(32, 32, 3)))   
+    model.add(Conv2D(16, (3, 3), strides = (1, 1), padding="same", name = "conv1", input_shape=(32, 32, 3)))   
     model.add(LeakyReLU(0.1))
     
     # CONV 2
-    model.add(Conv2D(32, (3, 3), strides = (1, 1), padding="same", name = 'conv2'))  
+    model.add(Conv2D(32, (3, 3), strides = (1, 1), padding="same", name = "conv2"))  
     model.add(LeakyReLU(0.1))
     
     # MaxPooling2D 1
-    model.add(MaxPooling2D((2, 2), name='max_pool_1'))
+    model.add(MaxPooling2D((2, 2), name="max_pool_1"))
     
     # Dropout
     model.add(Dropout(0.25, noise_shape=None, seed=0))
     
     # CONV 3
-    model.add(Conv2D(32, (3, 3), strides = (1, 1), padding="same", name = 'conv3')) 
+    model.add(Conv2D(32, (3, 3), strides = (1, 1), padding="same", name = "conv3")) 
     model.add(LeakyReLU(0.1))
     
     # CONV 4
-    model.add(Conv2D(64, (3, 3), strides = (1, 1), padding="same", name = 'conv4'))  
+    model.add(Conv2D(64, (3, 3), strides = (1, 1), padding="same", name = "conv4"))  
     model.add(LeakyReLU(0.1))
     
     # MaxPooling2D 1
-    model.add(MaxPooling2D((2, 2), name='max_pool_2'))
+    model.add(MaxPooling2D((2, 2), name="max_pool_2"))
     
     # Dropout
     model.add(Dropout(0.25, noise_shape=None, seed=0))
@@ -68,7 +68,7 @@ def make_model():
     model.add(Flatten())    
     
     # FC
-    model.add(Dense(256, name='fc1'))
+    model.add(Dense(256, name="fc1"))
     model.add(Dropout(0.5, noise_shape=None, seed=0))
     
     # FC
@@ -80,3 +80,38 @@ def make_model():
 K.clear_session() # Clear default graph.
 model = make_model()
 model.summary()
+
+# Train.
+INIT_LR = 5e-3  # initial learning rate
+BATCH_SIZE = 32
+EPOCHS = 10
+
+K.clear_session()  # clear default graph
+model = make_model()  # define our model
+
+# Prepare model for fitting (loss, optimizer, etc).
+model.compile(
+    loss="categorical_crossentropy",  # we train 10-way classification
+    optimizer=keras.optimizers.adamax(lr=INIT_LR),  # for SGD
+    metrics=["accuracy"]  # report accuracy during training
+)
+
+# scheduler of learning rate (decay with epochs)
+def lr_scheduler(epoch):
+    return INIT_LR * 0.9 ** epoch
+
+# callback for printing of actual learning rate used by optimizer
+class LrHistory(keras.callbacks.Callback):
+    def on_epoch_begin(self, epoch, logs={}):
+        print("Learning rate:", K.get_value(model.optimizer.lr))
+
+# Fit model.
+model.fit(
+    x_train2, y_train2,  # prepared data
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    callbacks=[keras.callbacks.LearningRateScheduler(lr_scheduler), LrHistory(), keras_utils.TqdmProgressCallback()],
+    validation_data=(x_test2, y_test2),
+    shuffle=True,
+    verbose=0
+)
