adc26bf857b171cd787db11343a8d9bfd2c65551
Syed Hussain Ather <shussainather@gmail.com>
2020-04-02 18:41:00 -0400

    make model

--- machinelearning/cnn.py
+++ machinelearning/cnn.py
@@ -28,3 +28,51 @@ x_test2 = x_test / 255. - 0.5
 # Convert class labels to one-hot encoded, should have shape (?, NUM_CLASSES).
 y_train2 = keras.utils.to_categorical(y_train, num_classes=NUM_CLASSES)
 y_test2 = keras.utils.to_categorical(y_test, num_classes=NUM_CLASSES)
+
+def make_model():
+    """
+    Returns `Sequential` model.
+    """
+    model = Sequential()
+
+    # CONV 1
+    # first layer needs to define "input_shape"
+    model.add(Conv2D(16, (3, 3), strides = (1, 1), padding="same", name = 'conv1', input_shape=(32, 32, 3)))   
+    model.add(LeakyReLU(0.1))
+    
+    # CONV 2
+    model.add(Conv2D(32, (3, 3), strides = (1, 1), padding="same", name = 'conv2'))  
+    model.add(LeakyReLU(0.1))
+    
+    # MaxPooling2D 1
+    model.add(MaxPooling2D((2, 2), name='max_pool_1'))
+    
+    # Dropout
+    model.add(Dropout(0.25, noise_shape=None, seed=0))
+    
+    # CONV 3
+    model.add(Conv2D(32, (3, 3), strides = (1, 1), padding="same", name = 'conv3')) 
+    model.add(LeakyReLU(0.1))
+    
+    # CONV 4
+    model.add(Conv2D(64, (3, 3), strides = (1, 1), padding="same", name = 'conv4'))  
+    model.add(LeakyReLU(0.1))
+    
+    # MaxPooling2D 1
+    model.add(MaxPooling2D((2, 2), name='max_pool_2'))
+    
+    # Dropout
+    model.add(Dropout(0.25, noise_shape=None, seed=0))
+    
+    # Flatten
+    model.add(Flatten())    
+    
+    # FC
+    model.add(Dense(256, name='fc1'))
+    model.add(Dropout(0.5, noise_shape=None, seed=0))
+    
+    # FC
+    model.add(Dense(NUM_CLASSES))  # the last layer with neuron for each class    
+    model.add(Activation("softmax"))  # output probabilities
+
+    return model
