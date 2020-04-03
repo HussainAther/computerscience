ff83f5a2efffe561f9023beeece718b7fc563349
Syed Hussain Ather <shussainather@gmail.com>
2020-04-03 01:20:28 -0400

    load

--- machinelearning/autoencoder.py
+++ machinelearning/autoencoder.py
@@ -11,3 +11,14 @@ from sklearn.model_selection import train_test_split
 """
 Denoising autoencoders 
 """
+
+# Load data.
+download_utils.link_week_4_resources()
+X, attr = load_lfw_dataset(use_raw=True, dimx=32, dimy=32)
+IMG_SHAPE = X.shape[1:]
+
+# Center images.
+X = X.astype('float32') / 255.0 - 0.5
+
+# Split.
+X_train, X_test = train_test_split(X, test_size=0.1, random_state=42)
