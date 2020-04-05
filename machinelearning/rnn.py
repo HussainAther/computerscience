76374c3886c17f3e864eeebd9c37bf692e11791b
Syed Hussain Ather <shussainather@gmail.com>
2020-04-04 22:47:32 -0400

    make layers

--- machinelearning/rnn.py
+++ machinelearning/rnn.py
@@ -22,3 +22,15 @@ def to_matrix(names, max_len=None, pad=0, dtype="int32"):
         names_ix[i,:len(name_ix)] = name_ix
 
     return names_ix.T
+
+rnn_num_units = 64
+embedding_size = 16
+
+# Create layers for our recurrent network.
+embed_x = Embedding(n_tokens,embedding_size) # an embedding layer that converts character ids into embeddings
+
+# a dense layer that maps input and previous state to new hidden state, [x_t,h_t]->h_t+1
+get_h_next = Dense(rnn_num_units, activation="tanh")
+
+# a dense layer that maps current hidden state to probabilities of characters [h_t+1]->P(x_t+1|h_t+1)
+get_probas = Dense(n_tokens, activation="softmax")
