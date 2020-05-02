11c67e5fda52d5a54d24301c9e5786727d3e4c6f
Syed Hussain Ather <shussainather@gmail.com>
2020-05-02 13:34:53 -0400

    build model

--- machinelearning/posrnn.py
+++ machinelearning/posrnn.py
@@ -1,3 +1,5 @@
+import keras
+import keras.layers as L
 import nltk
 import numpy as np
 import sys
@@ -64,3 +66,13 @@ def to_matrix(lines, token_to_id, max_len=None, pad=0, dtype="int32", time_major
 
 batch_words, batch_tags = zip(*[zip(*sentence) for sentence in data[-3:]])
 
+# Build model.
+model = keras.models.Sequential()
+model.add(L.InputLayer([None],dtype="int32"))
+model.add(L.Embedding(len(all_words),50))
+model.add(L.SimpleRNN(64,return_sequences=True))
+
+# Add top layer that predicts tag probabilities.
+stepwise_dense = L.Dense(len(all_tags),activation="softmax")
+stepwise_dense = L.TimeDistributed(stepwise_dense)
+model.add(stepwise_dense)
