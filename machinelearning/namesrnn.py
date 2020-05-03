5ef8dfa9cb257f7ecba57d4ff70adafbf2f84665
Syed Hussain Ather <shussainather@gmail.com>
2020-05-03 01:59:51 -0400

    open names

--- machinelearning/namesrnn.py
+++ machinelearning/namesrnn.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import tensorflow as tf
 
 """
 Generate names with recurrent neural networks.
 """
+
+start_token = " "
+
+with open("names") as f:
+    names = f.read()[:-1].split("\n")
+    names = [start_token+name for name in names]
