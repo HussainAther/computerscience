cbc9576421617505df1f6396d0f160e5382865ba
Syed Hussain Ather <shussainather@gmail.com>
2020-05-03 02:03:03 -0400

    to matrix

--- machinelearning/namesrnn.py
+++ machinelearning/namesrnn.py
@@ -30,5 +30,18 @@ tokens = list(tokens)
 n_tokens = len(tokens)
 print("n_tokens = ",n_tokens)
 
-assert 50 < n_tokens < 60
+token_to_id = {token:i for i,token in enumerate(tokens)}
 
+def to_matrix(names,max_len=None,pad=0,dtype="int32"):
+    """
+    Casts a list of names into rnn-digestable matrix.
+    """
+    
+    max_len = max_len or max(map(len,names))
+    names_ix = np.zeros([len(names),max_len],dtype) + pad
+
+    for i in range(len(names)):
+        name_ix = list(map(token_to_id.get,names[i]))
+        names_ix[i,:len(name_ix)] = name_ix
+
+    return names_ix.T
