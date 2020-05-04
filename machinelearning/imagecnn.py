ef69f7fda19dd600def5e04cdd69226e18b75989
Syed Hussain Ather <shussainather@gmail.com>
2020-05-04 12:02:47 -0400

    encoding architecture

--- machinelearning/imagecnn.py
+++ machinelearning/imagecnn.py
@@ -19,3 +19,15 @@ from collections import defaultdict
 from random import choice
 
 # Image captioning convolutional neural network (cnn)
+
+IMG_SIZE = 299
+
+def get_cnn_encoder():
+    """
+    Encoding architecture
+    """
+    K.set_learning_phase(0)
+    model = keras.applications.InceptionV3(include_top=False)
+    preprocess_for_model = keras.applications.inception_v3.preprocess_input
+    model = keras.engine.training.Model(model.inputs, keras.layers.GlobalAveragePooling2D()(model.output))
+    return model, preprocess_for_model
