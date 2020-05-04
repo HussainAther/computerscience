5083daec017ab5098448c0b05a401c66079abdfc
Syed Hussain Ather <shussainather@gmail.com>
2020-05-04 12:04:22 -0400

    load and sample

--- machinelearning/imagecnn.py
+++ machinelearning/imagecnn.py
@@ -31,3 +31,30 @@ def get_cnn_encoder():
     preprocess_for_model = keras.applications.inception_v3.preprocess_input
     model = keras.engine.training.Model(model.inputs, keras.layers.GlobalAveragePooling2D()(model.output))
     return model, preprocess_for_model
+
+# Load pre-trained model.
+K.clear_session()
+encoder, preprocess_for_model = get_cnn_encoder()
+
+# Extract train features.
+train_img_embeds, train_img_fns = utils.apply_model(
+    "train2014.zip", encoder, preprocess_for_model, input_shape=(IMG_SIZE, IMG_SIZE))
+utils.save_pickle(train_img_embeds, "train_img_embeds.pickle")
+utils.save_pickle(train_img_fns, "train_img_fns.pickle")
+
+# Extract validation features.
+val_img_embeds, val_img_fns = utils.apply_model(
+    "val2014.zip", encoder, preprocess_for_model, input_shape=(IMG_SIZE, IMG_SIZE))
+utils.save_pickle(val_img_embeds, "val_img_embeds.pickle")
+utils.save_pickle(val_img_fns, "val_img_fns.pickle")
+
+# Sample images for learners.
+def sample_zip(fn_in, fn_out, rate=0.01, seed=42):
+    np.random.seed(seed)
+    with zipfile.ZipFile(fn_in) as fin, zipfile.ZipFile(fn_out, "w") as fout:
+        sampled = filter(lambda _: np.random.rand() < rate, fin.filelist)
+        for zInfo in sampled:
+            fout.writestr(zInfo, fin.read(zInfo))
+            
+sample_zip("train2014.zip", "train2014_sample.zip")
+sample_zip("val2014.zip", "val2014_sample.zip")
